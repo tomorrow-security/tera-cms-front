05d74bc2ba2884618c00372df5f3fc50b1c02ed2
Alexia <alexia.skrzypczak@gmail.com>
2021-05-26 11:56:42 +0200

    liveLink and SocialLink modification

--- components/atoms/SocialLink.js
+++ components/atoms/SocialLink.js
@@ -1,13 +1,13 @@
 export default function SocialLink({ name, url, logo }) {
-  return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noreferrer"
-      className="flex items-center justify-center w-12 h-12 mx-1 rounded-full bg-tc-blue"
-      aria-label={name}
-    >
-      <div className="w-6 h-6 text-white">{logo}</div>
-    </a>
-  )
+    return (
+        <a
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center justify-center w-12 h-12 mx-1 rounded-full bg-tc-blue hover:bg-tc-red"
+            aria-label={name}
+        >
+          <div className="w-6 h-6 text-white">{logo}</div>
+        </a>
+    )
 }
