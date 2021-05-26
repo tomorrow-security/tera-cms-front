b564393c55130369aa4c87f0b2d0ece84f3ed603
Alexia <alexia.skrzypczak@gmail.com>
2021-05-26 17:07:48 +0200

    wip blockContact migration

--- pages/contact.js
+++ pages/contact.js
@@ -5,11 +5,13 @@ import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
 
+import BlockTitle from '../components/atoms/BlockTitle'
 import IconFacebook from '../components/atoms/IconFacebook'
 import IconInstagram from '../components/atoms/IconInstagram'
 import IconLinkedin from '../components/atoms/IconLinkedin'
 import IconTwitch from '../components/atoms/IconTwitch'
 import SocialLink from '../components/atoms/SocialLink'
+import IconCurvedArrowX from '../components/atoms/IconCurvedArrowX'
 
 const pageTitle = "Contact - Tera Campus"
 const pageDescription = "Contacter Tera Campus"
@@ -39,39 +41,63 @@ export default function Contact() {
       </Head>
 
       <main>
-        <section className="pt-8 md:py-20">
+        <section id="contact" className="py-10 md:py-20">
           <div className="md:container md:mx-auto">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-              <div className="mx-2 flex flex-col space-y-8">
-                <div className="space-y-4">
-                  <div className="flex justify-center space-x-2">
-                    <SocialLink
-                      name="linkedin"
-                      url="https://www.linkedin.com/company/teracampus"
-                      logo={<IconLinkedin />}
-                    />
-                    <SocialLink
-                      name="facebook"
-                      url="https://www.facebook.com/teracampus"
-                      logo={<IconFacebook />}
-                    />
-                    <SocialLink
-                      name="instagram"
-                      url="https://www.instagram.com/tera.campus"
-                      logo={<IconInstagram />}
-                    />
-                    <SocialLink
-                      name="twitch"
-                      url="https://www.twitch.tv/teracampus"
-                      logo={<IconTwitch />}
-                    />
-                  </div>
-                  <div className="text-center">
-                    <a href="mailto:contact@tera-campus.com" className="text-xl text-blue-600 underline">
-                      contact@tera-campus.com
-                    </a>
+            <BlockTitle
+              title="Contact"
+              iconPicture={{ backgroundImage: "url('mobile-sms-blue.png')" }}
+            />
+            <div className="flex flex-col mx-2 flex-nowrap md:flex-row-reverse md:justify-around md:items-center">
+              <div className="flex flex-col w-full p-2 space-y-2 md:w-1/2">
+                <div className="flex flex-row items-center justify-center space-x-4 flex-nowrap">
+                  <SocialLink
+                    name="linkedin"
+                    url="https://www.linkedin.com/company/teracampus"
+                    logo={<IconLinkedin />}
+                  />
+                  <SocialLink
+                    name="facebook"
+                    url="https://www.facebook.com/teracampus"
+                    logo={<IconFacebook />}
+                  />
+                  <SocialLink
+                    name="instagram"
+                    url="https://www.instagram.com/tera.campus"
+                    logo={<IconInstagram />}
+                  />
+                  <SocialLink
+                    name="twitch"
+                    url="https://www.twitch.tv/teracampus"
+                    logo={<IconTwitch />}
+                  />
+                </div>
+                <div className="flex flex-row items-start flex-nowrap">
+                  <div className="w-4 h-4 m-2 transform rotate-45 bg-tc-red"> </div>
+                  <div className="flex flex-col w-11/12 ">
+                    <p className="pt-1 pl-2">Tera Campus répond à tes questions</p>
+                    <div className="relative mx-auto">
+                      <a
+                        href="mailto:contact@tera-campus.com"
+                        className="p-2 mx-auto text-sm text-center underline w-max md:text-lg hover:text-tc-red">
+                        contact@tera-campus.com
+                      </a>
+                    </div>
                   </div>
                 </div>
+                <div className="my-4">
+                  <div className="flex flex-row items-start justify-start flex-nowrap">
+                    <div className="w-4 h-4 m-2 transform rotate-45 bg-tc-red"> </div>
+                    <p className="w-11/12 pt-1 pl-2">
+                      Pose tes questions directement via le formulaire
+                    </p>
+                  </div>
+                  <div className="m-2 animate-bounce">
+                    <div className="w-8 h-8 mx-auto transform -rotate-60">
+                      <IconCurvedArrowX />
+                    </div>
+                  </div>
+                </div>
+                {/* // TODO design form à migrer */}
                 <form className="grid grid-cols-1 gap-6" onSubmit={handleSubmit(onSubmit)}>
                   <label className="block">
                     <span>Ton adresse e-mail :</span>
@@ -127,7 +153,7 @@ export default function Contact() {
                   />
                 </form>
               </div>
-              <div className="relative min-w-min flex">
+              <div className="relative flex min-w-min">
                 <Image
                   src="/happy-woman-phone.jpg"
                   alt="Femme utilisant un téléphone"
@@ -135,8 +161,8 @@ export default function Contact() {
                   height={768}
                   className="object-cover object-center"
                 />
-              </div>
             </div>
+            </div >
           </div>
         </section>
       </main>
