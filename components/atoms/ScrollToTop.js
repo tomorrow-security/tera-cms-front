88a255c92897fcd174d2f4832285fb8d65ef8d08
Alexia <alexia.skrzypczak@gmail.com>
2021-05-26 11:01:20 +0200

    atom migration ScrollToTop

---
+++ components/atoms/ScrollToTop.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
+    return null
+}
