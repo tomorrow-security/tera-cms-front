c36825f6a16db9d0c89ac32eb55682e2280aaaaf
Alexia <alexia.skrzypczak@gmail.com>
2021-05-27 14:57:29 +0200

    fix button

--- components/organisms/Hero.js
+++ components/organisms/Hero.js
@@ -20,21 +20,19 @@ export default function Hero() {
                 </div>
                 <div className="mt-10 space-y-10 text-center">
                     <p className="mb-8 text-xl sm:text-2xl md:text-2xl">
-              École supérieure d&apos;informatique en télé-présentiel
+                        École supérieure d&apos;informatique en télé-présentiel
                     </p>
-                    <div
-                        // TODO adapté le lien à next.js to="/enrolment"
-                        className="box-border flex items-center w-4/5 p-4 mx-auto mt-8 tracking-normal text-white rounded-full justify-evenly 2xl:w-9/12 xl:mb-12 md:mt-0 lg:mt-20 xl:mt-10 2xl:mt-40 bg-tc-blue hover:bg-tc-red"
-                    >
-                        <Link href="/enrolment">
-                            <a className="text-base font-bold uppercase align-text-top md:text-2xl animate-pulse">
+                    <Link href="/enrolment">
+                        <a className="text-base font-bold uppercase align-text-top md:text-2xl animate-pulse">
+                            <div
+                            className="box-border flex items-center w-4/5 p-4 mx-auto mt-8 tracking-normal text-white rounded-full justify-evenly 2xl:w-9/12 xl:mb-12 md:mt-0 lg:mt-20 xl:mt-10 2xl:mt-40 bg-tc-blue hover:bg-tc-red">
                                 Je m&apos;inscris
-                            </a>
-                        </Link>
-                        <div className="pl-2 text-2xl sm:pl-4 md:pl-6 sm:text-4xl md:text-5xl animate-pulse">
-                            &rarr;
-                        </div>
-                    </div>
+                                <div className="pl-2 text-2xl sm:pl-4 md:pl-6 sm:text-4xl md:text-5xl animate-pulse">
+                                    &rarr;
+                                </div>
+                            </div>
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
