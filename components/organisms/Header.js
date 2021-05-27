59dc752610cdaf925b748de4b713f98f428a2866
Alexia <alexia.skrzypczak@gmail.com>
2021-05-27 15:15:40 +0200

    removal of the Nav molecule to integrate it into the BlockHeader => fix layout view mobile

--- components/organisms/Header.js
+++ components/organisms/Header.js
@@ -2,7 +2,24 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 import IconLogo from '../atoms/IconLogo'
-import Nav from '../molecules/Nav'
+import IconX from '../atoms/IconX'
+import IconMenu from '../atoms/IconMenu'
+
+const links1 = [
+    { id: 'agenda', label: 'Agenda' },
+]
+
+const links2 = [
+    { id: 'faq', label: 'Questions' },
+]
+
+const pages = [
+    { id: 'career', label: 'Carrières' },
+    { id: 'guides', label: 'Nos guides' },
+    { id: 'program', label: 'Programme' },
+    { id: 'join', label: 'Rejoins-nous' },
+    { id: 'contact', label: 'Contact' },
+]
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false)
@@ -11,7 +28,7 @@ export default function Header() {
     <header className="py-2 text-white border-b-2 bg-tc-blue border-tc-red">
       <div className="md:container md:mx-auto">
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="flex items-center justify-between w-full lg:w-full">
+          <div className="flex items-center justify-between w-full lg:w-max">
             <Link href="/">
               <a className="flex items-center pl-2">
                 <div className="w-12 mr-4">
@@ -27,8 +44,56 @@ export default function Header() {
                 </div>
               </a>
             </Link>
-            <Nav />
+                <div className="w-12 lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+                { isOpen ? <IconX /> : <IconMenu /> }
+            </div>
           </div>
+          <nav className={`${!isOpen && 'hidden'} lg:flex lg:justify-between text-center pr-2`}>
+              <ul className="lg:flex">
+                  {
+                      links1.map(({ id, label }) => (
+                          <li key={`navmd-${id}`} onClick={() => setIsOpen(false)}>
+                              <Link href={`/#${id}`}>
+                                  <a>
+                                      <div className="my-2 text-lg border-b-2 border-transparent cursor-pointer lg:my-0 lg:px-4 hover:border-white">
+                                          {label}
+                                      </div>
+                                  </a>
+                              </Link>
+                          </li>
+                      ))
+                  }
+              </ul>
+              <ul className="lg:flex">
+                  {
+                      pages.map(({ id, label }) => (
+                          <li key={`navmd-${id}`} onClick={() => setIsOpen(false)}>
+                              <Link href={`/${id}`}>
+                                  <a className="my-2 text-lg border-b-2 border-transparent cursor-pointer lg:my-0 lg:px-4 hover:border-white"
+                                  >
+                                      {label}
+                                  </a>
+                              </Link>
+                          </li>
+                      ))
+                  }
+              </ul>
+              <ul className="lg:flex">
+                  {
+                      links2.map(({ id, label }) => (
+                          <li key={`navmd-${id}`} onClick={() => setIsOpen(false)}>
+                              <Link href={`/#${id}`}>
+                                  <a>
+                                      <div className="my-2 text-lg border-b-2 border-transparent cursor-pointer lg:my-0 lg:px-4 hover:border-white">
+                                          {label}
+                                      </div>
+                                  </a>
+                              </Link>
+                          </li>
+                      ))
+                  }
+              </ul>
+          </nav>
         </div>
       </div>
     </header>
