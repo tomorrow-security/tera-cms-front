77b5fe0ae6e77f30dbce30b3e49b79e309f2d3a4
Alexia <alexia.skrzypczak@gmail.com>
2021-05-28 12:22:03 +0200

    comment TODO Footer

--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -2,26 +2,29 @@ import Link from 'next/link'
 
 import IconLogo from '../atoms/IconLogo'
 
+// TODO ajouter du style au liens visité
+// TODO améliorer le style du hover sur les lien
+// TODO ajouter du style au liens actif
 export default function Footer() {
   const today = new Date()
 
   return (
-    <footer className="py-10 bg-tc-blue text-white">
-      <div className="relative md:container mx-2 md:mx-auto md:px-2">
-        <div className="flex flex-col lg:flex-row justify-between items-center">
+    <footer className="py-10 text-white bg-tc-blue">
+      <div className="relative mx-2 md:container md:mx-auto md:px-2">
+        <div className="flex flex-col items-center justify-between lg:flex-row">
           <div className="flex items-center">
               <div className="w-10 mr-2"><IconLogo /></div>
               <span className="text-xl">Tera Campus</span>
           </div>
-          <div className="my-8 lg:my-0 flex flex-col lg:flex-row text-center">
+          <div className="flex flex-col my-8 text-center lg:my-0 lg:flex-row">
           <Link href="/contact">
               <a className="m-2 hover:underline">Contact</a>
             </Link>
-            <span className="hidden lg:block m-2">-</span>
+            <span className="hidden m-2 lg:block">-</span>
             <Link href="/legal">
               <a className="m-2 hover:underline">Mentions légales</a>
             </Link>
-            <span className="hidden lg:block m-2">-</span>
+            <span className="hidden m-2 lg:block">-</span>
             <Link href="/privacy">
               <a className="m-2 hover:underline">Politique de confidentialité</a>
             </Link>
