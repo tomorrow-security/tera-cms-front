e1611ce5a3095a03413e91518d641b66edbd10f4
Alexia <alexia.skrzypczak@gmail.com>
2021-06-01 14:52:55 +0200

    addition of the hexadecimal symbol of the link on the external link of BlockJoin and removal of the atom IconExternalLink

--- components/organisms/BlockJoin.js
+++ components/organisms/BlockJoin.js
@@ -122,7 +122,7 @@ export default function BlockJoin() {
                 </div>
                 <div className="flex justify-center pt-8 my-2">
                      {/* // TODO ajouter visited */}
-                    <a href="https://www.alternance.emploi.gouv.fr/portail_alternance/" className="relative z-10 w-full overflow-hidden transition duration-700 rounded-t h-7 md:h-9 md:w-3/5 group md:text-xl bg-tc-blue rounded-b-xl">
+                    <a href="https://www.alternance.emploi.gouv.fr/portail_alternance/" className="relative z-10 w-full overflow-hidden transition duration-700 rounded-t h-7 md:h-9 md:w-4/5 group md:text-xl bg-tc-blue rounded-b-xl">
                         <span className="absolute left-0 z-20 h-full transition-all delay-100 transform rounded-full bg-tc-red w-1/8 -bottom-full group-hover:-translate-y-full group-hover:scale-200"></span>
                         <span className="absolute z-20 h-full transition-all ease-out delay-200 transform rounded-full w-1/8 -bottom-full left-1/8 group-hover:-translate-y-full bg-tc-red group-hover:scale-200"></span>
                         <span className="absolute z-20 h-full transition-all ease-in delay-300 transform rounded-full w-1/8 -bottom-full left-2/8 group-hover:-translate-y-full bg-tc-red group-hover:scale-200"></span>
@@ -131,7 +131,8 @@ export default function BlockJoin() {
                         <span className="absolute z-20 h-full transition-all ease-in transform rounded-full delay-600 w-1/8 -bottom-full left-5/8 group-hover:-translate-y-full bg-tc-red group-hover:scale-200"></span>
                         <span className="absolute z-20 h-full transition-all ease-in delay-700 transform rounded-full w-1/8 -bottom-full left-6/8 group-hover:-translate-y-full bg-tc-red group-hover:scale-200"></span>
                         <span className="absolute z-20 h-full transition-all ease-in transform rounded-full w-1/8 -bottom-full left-7/8 delay-800 group-hover:-translate-y-full bg-tc-red group-hover:scale-200"></span>
-                        <p className="absolute z-30 inline-block w-full h-full font-semibold text-center text-white align-bottom">Comment trouver une entreprise ?</p>
+                        <p className="absolute z-30 inline-block w-full h-full font-semibold text-center text-white align-bottom">Comment trouver une entreprise ? &#x1F517;
+                        </p>
                     </a>
                 </div>
             </>
