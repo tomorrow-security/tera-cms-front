93c4a56a9f7c30d0f458f07242a6473f3abc28b4
Alexia <alexia.skrzypczak@gmail.com>
2021-06-02 18:21:07 +0200

    creation of the CarouselPartners molecule component WIP

---
+++ components/molecules/CarouselPartners.js
@@ -0,0 +1,12 @@
+export default function CarouselPartners(array) {
+    return (
+        <div className="relative overflow-hidden md:full">
+                    <div className="md:top-0 md:left-0 md:absolute md:bg-carousel md:z-10 md:h-full md:w-200px"></div>
+                    <div className="flex flex-wrap items-center justify-center my-8 md:flex-nowrap md:w-carousel md:justify-between md:animate-scroll">
+                {`${array}`}
+                {`${array}`}
+                    </div>
+                    <div className="md:absolute md:top-0 md:right-0 md:transform md:rotate-180 md:bg-carousel md:h-full md:w-200px md:z-10"></div>
+                </div>
+    )
+}
