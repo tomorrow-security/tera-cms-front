3f294fd53e9f0a674d559a7ee482a2303a928c3b
Alexia <alexia.skrzypczak@gmail.com>
2021-06-07 17:19:57 +0200

    mav of the footer with a map and border-top in focus

--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 
 import IconLogo from '../atoms/IconLogo'
+const pages = [
+    { id: 'contact', label: 'Contact' },
+    { id: 'legal', label: 'Mentions légales' },
+    { id: 'privacy', label: 'Politique de confidentialité' },
+]
 
 // TODO ajouter du style au liens visité
-// TODO ajouter du style au liens actif
 export default function Footer() {
   const today = new Date()
 
@@ -15,39 +19,25 @@ export default function Footer() {
               <div className="w-10 mr-2"><IconLogo /></div>
               <span className="text-xl">Tera Campus</span>
           </div>
-          <div className="flex flex-col my-8 text-center lg:my-0 lg:flex-row">
-          <Link href="/contact">
-              <a className="relative flex items-center justify-center m-2 overflow-hidden group border-box ">
-                  <div className="absolute z-10 text-base text-transparent transition-all duration-300 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
-                      Contact
-                  </div>
-                  <div className="z-20 text-base bg-transparent border-b-2 border-transparent cursor-pointer">
-                      Contact
-                  </div>
-              </a>
-            </Link>
-            <span className="hidden m-2 lg:block">-</span>
-            <Link href="/legal">
-              <a className="relative flex items-center justify-center m-2 overflow-hidden group border-box ">
-                  <div className="absolute z-10 text-base text-transparent transition-all duration-300 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
-                      Mentions légales
-                  </div>
-                  <div className="z-20 text-base bg-transparent border-b-2 border-transparent cursor-pointer">
-                      Mentions légales
-                  </div>
-              </a>
-            </Link>
-            <span className="hidden m-2 lg:block">-</span>
-            <Link href="/privacy">
-              <a className="relative flex items-center justify-center m-2 overflow-hidden group border-box ">
-                  <div className="absolute z-10 text-base text-transparent transition-all duration-300 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
-                      Politique de confidentialité
-                  </div>
-                  <div className="z-20 text-base bg-transparent border-b-2 border-transparent cursor-pointer">
-                      Politique de confidentialité
-                  </div>
-              </a>
-            </Link>
+          <div className="flex flex-col my-8 text-center divide-x lg:my-0 lg:flex-row">
+            <ul className="space-y-2 lg:mx-4 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-center lg:divide-x">
+              {
+                pages.map(({ id, label }) => (
+                  <li key={`navmd-${id}`}>
+                    <Link href={`/${id}`}>
+                      <a className="relative flex items-center justify-center m-2 overflow-hidden group border-box focus:border-t-2 focus:border-tc-red">
+                        <div className="absolute z-10 text-base text-transparent transition-all duration-300 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
+                          {label}
+                      </div>
+                      <div className="z-20 text-base bg-transparent border-b-2 border-transparent cursor-pointer">
+                          {label}
+                      </div>
+                      </a>
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
           </div>
           <div className="text-center">© 2020-{today.getFullYear()} Tera Campus</div>
         </div>
