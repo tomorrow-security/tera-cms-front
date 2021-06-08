88d39a7597aca35b4ba17d1e0e4898dde927f886
Alexia <alexia.skrzypczak@gmail.com>
2021-06-08 15:35:24 +0200

    style hover link

--- components/atoms/HashLink.js
+++ components/atoms/HashLink.js
@@ -7,7 +7,7 @@ export default function HashLink({ id, label }) {
 	return (
 		<Link href={`/#${id}`}>
 			<a className="relative flex items-center justify-center overflow-hidden ease-in transform group border-box focus:font-bold">
-				<div className="absolute z-10 text-lg text-transparent transition-all duration-300 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
+				<div className="absolute z-10 text-lg text-transparent transition-all duration-200 ease-in transform border-b-2 border-transparent border-white cursor-pointer -left-full group-hover:translate-x-full">
 					{label}
 				</div>
 				<div className="z-20 text-lg bg-transparent border-b-2 border-transparent cursor-pointer">
