169a84178120331407d1e08528bce97b078cd125
Alexia <alexia.skrzypczak@gmail.com>
2021-06-10 14:48:12 +0200

    fix HashLink

--- components/atoms/HashLink.js
+++ components/atoms/HashLink.js
@@ -25,8 +25,8 @@ export default function HashLink({ id, label }) {
 	return (
 		<Link href={href}>
 			<a className={`${active} relative flex items-center justify-center overflow-hidden ease-in transform group border-box active:font-extrabold`}>
-				<div className="absolute z-10 text-lg text-transparent transition-all duration-200 ease-in transform border-b-2 border-current cursor-pointer -left-full group-hover:translate-x-full">
-					{label}
+				<div className="absolute z-10 text-lg transition-all duration-200 ease-in transform border-b-2 border-current cursor-pointer -left-full group-hover:translate-x-full">
+					<p className="text-transparent">{label}</p>
 				</div>
 				<div className="z-20 text-lg underline bg-transparent border-b-2 border-transparent cursor-pointer group-hover:no-underline">
 					{label}
