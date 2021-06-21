1110a21a9da2afe66bba5069e64b76eb64318e46
Alexia <alexia.skrzypczak@gmail.com>
2021-06-21 16:48:04 +0200

    layout

--- components/molecules/Input.js
+++ components/molecules/Input.js
@@ -9,15 +9,17 @@ export default function Input({
 }) {
   
     return (
-        <div className="flex space-x-2">
-          <div className="flex items-center flex-1 space-around">
-            <label
-              htmlFor={id}
-              // className={`flex-1 p-2 ${getBorder(invalid, isSelected)} text-center cursor-pointer`}
-              className="flex-1 p-2 text-center cursor-pointer"
-            >
+        <div className="flex mx-auto space-x-2">
+          <div className="flex items-center justify-between flex-1 w-full lg:w-2/3">
+            <div className="min-content">
+              <label
+                htmlFor={id}
+                // className={`flex-1 p-2 ${getBorder(invalid, isSelected)} text-center cursor-pointer`}
+                className="flex-1 p-2 text-center cursor-pointer"
+              >
               {label}
             </label>
+            </div>
             <input
             type={type}
             id={id}
@@ -26,13 +28,13 @@ export default function Input({
             autoComplete={id}
             required={required}
               // ref={ref}
-            className="w-3/4 p-2 border rounded outline-none border-1"
+            className="w-2/3 p-2 border rounded outline-none lg:max-w-md border-1"
             // className={`
             //     w-full p-2 border
             //     ${errors.email ? 'border-tc-red' : 'border-tc-blue'}
             //     rounded-none outline-none
             // `}
-          />
+            />
           </div>
         </div>
     )
