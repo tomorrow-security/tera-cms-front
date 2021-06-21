d71c4278d9d041f4c877c872a29bd796cbe880a0
Alexia <alexia.skrzypczak@gmail.com>
2021-06-21 16:09:01 +0200

    provisional API for enrollment

---
+++ pages/api/enrolment.js
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ user: 'Ada Lovelace' })
+}
+
+export default (req, res) => {
+  res.status(200).json({ name: 'John Doe' })
+}
+
+export default (req, res) => {
+  const mailjet = require('node-mailjet')
+    .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+  
+  const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [{
+        From: {
+          Email: 'contact@tera-campus.com',
+          Name: 'Formulaire de contact',
+        },
+        ReplyTo: { Email: req.body.email },
+        To: [{
+          Email: 'contact@tera-campus.com',
+          Name: 'Tera Campus',
+        }],
+        Subject: 'Demande d\'informations à propos de Tera Campus',
+        TextPart: req.body.message,
+      }],
+      SandboxMode: process.env.NODE_ENV === 'development',
+    })
+  
+  request
+    .then(() => res.status(200).send())
+    .catch(() => res.status(500).send())
+}
