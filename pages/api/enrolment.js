95a7268fb3bf6ce06f6aade4a7aaa16b041dc63f
Alexia <alexia.skrzypczak@gmail.com>
2021-06-29 10:43:59 +0200

    wip api/enrolment

--- pages/api/enrolment.js
+++ pages/api/enrolment.js
@@ -2,22 +2,18 @@ export default function handler(req, res) {
   // l'exemple de la doc
   // res.status(200).json({ text: 'Hello' })
 
-  const enrolment1= connect (process.env.API_URL)
+  const api = connect(process.env.API_URL)
+  const apiEnrolment1 = `{${API_URL}/public/new}`
 
   const email = req.body.email
-  const gender = req.body.gender,
-  const firstName = req.body.firstName,
-  const lastName = req.body.lastName,
-  const phone = req.body.phone,
+  const gender = req.body.gender
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const phone = req.body.phone
   const consent = req.body.consentContact
 
-  const api = process.env.API_URL
-
-  //ne fonctionne pas 
-  // const api = process.env.API_URL/public/new
-
-  const request = enrolment1
-    .post()
+  const request = api
+    .post({apiEnrolment1})
     .request({
       email,
       gender,
