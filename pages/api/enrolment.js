444531084b3d24fd79f0971c29599ff91f4cedfb
Alexia <alexia.skrzypczak@gmail.com>
2021-06-29 10:39:38 +0200

    WIP api/enrolment

--- pages/api/enrolment.js
+++ pages/api/enrolment.js
@@ -1,18 +1,33 @@
-export default (req, res) => {
+export default function handler(req, res) {
+  // l'exemple de la doc
+  // res.status(200).json({ text: 'Hello' })
+
+  const enrolment1= connect (process.env.API_URL)
+
+  const email = req.body.email
+  const gender = req.body.gender,
+  const firstName = req.body.firstName,
+  const lastName = req.body.lastName,
+  const phone = req.body.phone,
+  const consent = req.body.consentContact
+
   const api = process.env.API_URL
 
-  const request = api
+  //ne fonctionne pas 
+  // const api = process.env.API_URL/public/new
+
+  const request = enrolment1
     .post()
     .request({
-      email: req.body.email,
-      gender: req.body.gender,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phone: req.body.phone,
-      consent: req.body.consentContact
+      email,
+      gender,
+      firstName,
+      lastName,
+      phone,
+      consent
     })
   
   request
-    .then(() => res.status(200).send())
+    .then(() => res.status(200).send({"redirectUrl": "https://tera-campus.com/enrolment/quiz/<UUID>"}))
     .catch(() => res.status(500).send())
 }
