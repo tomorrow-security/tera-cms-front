fcddc9f54bf6b8bc1f7fa2325322a9ae580b1d57
Alexia <alexia.skrzypczak@gmail.com>
2021-07-27 15:53:45 +0200

    modification of BrochureButton text

--- components/atoms/BrochureButton.js
+++ components/atoms/BrochureButton.js
@@ -7,7 +7,7 @@ export default function BrochureButton() {
                 <a className="mx-auto my-4 transition-colors duration-700 border-4 rounded-t animate-none z-20max-w-3xl group border-tc-blue-xlight hover:border-tc-red-xlight bg-tc-blue hover:bg-tc-red rounded-b-xl">
                     {/* // TODO retirer le bg-shine au hover : 'group-hover:bg-none' ne fonctionne pas */}
                     <p className="p-2 text-xl font-bold text-center text-white sm:px-6">
-                        Recevoir la brochure
+                        Télécharger la brochure
                     </p>
                 </a>
             </Link >
