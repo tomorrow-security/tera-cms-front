2ee4cad8f9aa509639f6cdbe4a01a9bb4b807969
Alexia <alexia.skrzypczak@gmail.com>
2021-07-28 16:24:29 +0200

    creation of the InputButton component without mutation

--- components/molecules/InputButton.js
+++ components/molecules/InputButton.js
@@ -1,43 +1,46 @@
-export default function InputButton({mutation}) {
-  
-  const submitButtonParams = {
-    loading: {
-      value: 'En cours d\'envoi ...',
-      color: 'bg-tc-red-medium',
-      position:'absolute left-0 top-0 z-20',
-      shadow: 'bg-tc-red-xlight border border-tc-red-dark border-t-0',
-      disabled: true
-    },
-    success: {
-      value: 'Envoyé !',
-      color: 'bg-tc-blue-medium',
-      position:'absolute left-0 -top-20/100 z-20',
-      shadow: 'bg-tc-blue-xlight border border-tc-blue border-t-0',
-      disabled: true
-    },
-    default: {
-      value: 'Envoyer',
-      color: 'bg-tc-blue group-hover:bg-tc-red shadow group-hover:shadow-none cursor-pointer  transition-colors duration-700 group-hover:-top-20/100 transform active:translate-y-20/100',
-      position:'absolute left-0 -top-15/100 z-20',
-      shadow: 'bg-tc-blue-light border border-tc-blue-dark group-hover:bg-tc-red-light group-hover:border-tc-red-dark shadow group-hover:shadow-none cursor-pointer  transition-colors duration-700',
-      disabled: false
-    }
-  }
-
-  // TODO revoir de submit style hover plus cibler sur le button
-
-    return (
-       <div className="pt-2 mx-auto w-max group">
-        <div className={`relative z-0 px-4 font-bold text-transparent rounded-t outline-none cursor-pointer w-max rounded-b-xl ${['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].shadow : submitButtonParams['default'].shadow}`}>
-          {['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].value : submitButtonParams['default'].value}
-          <input
-          type="submit"
-          value={['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].value : submitButtonParams['default'].value}
-          className={`${['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].position : submitButtonParams['default'].position} w-max px-4 mx-auto
-              outline-none cursor-pointer text-white font-bold rounded-t rounded-b-xl ${['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].color : submitButtonParams['default'].color}`}
-          disabled={['loading', 'success'].includes(mutation.status) ? submitButtonParams[mutation.status].disabled : submitButtonParams['default'].disabled }
-          />
+export default function InputButton({loadingValue, successValue, defaultValue }) {
+    
+  switch (status) {
+    case 'loading':
+      return (
+        <div className="w-auto pt-2 mx-auto group">
+          <div className="relative z-0 h-10 px-4 font-bold text-transparent border border-t-0 rounded-t outline-none cursor-pointer w-max rounded-b-xl bg-tc-red-xlight border-tc-red-dark">
+            {loadingValue}
+            <input
+              type="submit"
+              value={loadingValue}
+              className="absolute top-0 left-0 z-20 h-10 px-4 mx-auto font-bold text-white rounded-t outline-none cursor-pointer w-max rounded-b-xl bg-tc-red-medium"
+              disabled
+            />
           </div>
-      </div>
-    )
+        </div>
+      )
+    case 'success':
+      return (
+        <div className="w-auto pt-2 mx-auto group">
+          <div className="relative z-0 h-10 px-4 font-bold text-transparent border border-t-0 rounded-t outline-none cursor-pointer w-max rounded-b-xl bg-tc-blue-xlight border-tc-blue">
+            {successValue}
+            <input
+              type="submit"
+              value={successValue}
+              className="absolute left-0 z-20 h-10 px-4 mx-auto font-bold text-white rounded-t outline-none cursor-pointer bg-tc-blue-medium -top-20/100 w-max rounded-b-xl"
+              disabled
+            />
+          </div>
+        </div>
+      )
+    default:
+      return (
+        <div className="w-auto pt-2 mx-auto group">
+          <div className="relative z-0 h-10 px-4 font-bold text-transparent transition-colors duration-700 border rounded-t shadow outline-none cursor-pointer w-max rounded-b-xl bg-tc-blue-light border-tc-blue-dark group-hover:bg-tc-red-light group-hover:border-tc-red-dark group-hover:shadow-none">
+            {defaultValue}
+            <input
+              type="submit"
+              value={defaultValue}
+              className="absolute left-0 z-20 h-10 px-4 mx-auto font-bold text-white transition-colors duration-700 transform rounded-t shadow outline-none cursor-pointer -top-15/100 w-max rounded-b-xl bg-tc-blue group-hover:bg-tc-red group-hover:shadow-none group-hover:-top-20/100 active:translate-y-20/100"
+            />
+          </div>
+        </div>
+      )
+  }
 }
