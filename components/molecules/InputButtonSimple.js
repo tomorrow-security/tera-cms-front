731ef3a328707da52cf085b271ebd785694fd865
Alexia <alexia.skrzypczak@gmail.com>
2021-07-28 16:38:28 +0200

    creation of the InputButtonSimple molecule

---
+++ components/molecules/InputButtonSimple.js
@@ -0,0 +1,15 @@
+export default function InputButtonSimple({ value, onClick }) {
+  return (
+  <div className="pt-8 mx-auto w-max group">
+    <div className="relative z-0 h-10 px-4 mx-auto font-bold text-transparent transition-colors duration-700 border rounded-t shadow outline-none cursor-pointer w-max rounded-b-xl bg-tc-blue-light border-tc-blue-dark group-hover:bg-tc-red-light group-hover:border-tc-red-dark group-hover:shadow-none">
+      {value}
+      <input
+        type="submit"
+        value={value}
+        className="absolute left-0 z-20 h-10 px-4 mx-auto font-bold text-white transition-colors duration-700 transform rounded-t shadow outline-none cursor-pointer -top-15/100 w-max rounded-b-xl bg-tc-blue group-hover:bg-tc-red group-hover:shadow-none group-hover:-top-20/100 active:translate-y-20/100"
+        onClick={onClick}
+      />
+    </div>
+    </div>
+  )
+}
