4bde37773d5f5cf405df45bfa917e91bf32ec17c
Alexia <alexia.skrzypczak@gmail.com>
2021-07-28 17:00:50 +0200

    using InputButtonMutation in EnrolmentTest for testEnded

--- components/organisms/EnrolmentTest.js
+++ components/organisms/EnrolmentTest.js
@@ -218,6 +218,9 @@ const TestEnded = ({ applicant, test, uuid, setPageData }) => {
           </div>
           <div className="mt-12">
             <InputButtonMutation
+              // defaultValue="Envoyer"
+              // loadingValue="En cours d'envoi..."
+              // successValue="Envoyé !"
               mutation={mutation}
             />
           </div>
