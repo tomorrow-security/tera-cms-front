2d71c4dbf58f871a193c28b74c50eb8b67c58f4a
Brice AIELLO <brice.aiello@me.com>
2021-08-03 17:48:14 +0200

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
+import { FB_PIXEL_ID } from '../lib/fpixel'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -12,6 +13,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `
+          }} />
           {/* Facebook Pixel */}
           <script dangerouslySetInnerHTML={{
             __html: `
