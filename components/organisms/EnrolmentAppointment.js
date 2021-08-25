22c2690431a332343ae2e0b92324b68202cff93b
Alexia <alexia.skrzypczak@gmail.com>
2021-08-25 17:19:56 +0200

    Layout EnrolmentAppointment

--- components/organisms/EnrolmentAppointment.js
+++ components/organisms/EnrolmentAppointment.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from "react"
 
 export default function EnrolmentAppointment() {
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://assets.calendly.com/assets/external/widget.js'
-    script.async = true
-    document.body.appendChild(script)
-  }, [])
+	useEffect(() => {
+		const script = document.createElement("script")
+		script.src = "https://assets.calendly.com/assets/external/widget.js"
+		script.async = true
+		document.body.appendChild(script)
+	}, [])
 
-  return (
-    <>
-      <div className="mb-4 text-xl text-center md:text-2xl">
-        Pour terminer ton inscription, planifies ton entretien de 15mn avec Tera Campus.
-      </div>
-      <div className="xl:my-12">
-        <div
-          className="w-full min-h-full calendly-inline-widget"
-          data-url="https://calendly.com/lilian-simon/ton-entretien-personnalise-avec-tera-campus?text_color=000000&hide_event_type_details=1"
-          style={{ minWidth: '320px', height: '650px' }}
-        />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="mx-4 mb-4 text-xl text-center md:text-2xl">
+				Pour terminer ton inscription, planifies ton entretien de 15mn avec Tera
+				Campus.
+			</div>
+			<div className="xl:my-12">
+				<div
+					className="max-w-full min-h-full calendly-inline-widget"
+					data-url="https://calendly.com/lilian-simon/ton-entretien-personnalise-avec-tera-campus?text_color=000000&hide_event_type_details=1"
+					style={{ minWidth: "320px", height: "650px" }}
+				/>
+			</div>
+		</>
+	)
 }
