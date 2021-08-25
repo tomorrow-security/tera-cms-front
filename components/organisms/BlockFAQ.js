aa84308ee7e4d8fb608db076238921799849c45a
Alexia <alexia.skrzypczak@gmail.com>
2021-08-25 15:59:34 +0200

    indentation with Prettier de BlockFAQ

--- components/organisms/BlockFAQ.js
+++ components/organisms/BlockFAQ.js
@@ -1,59 +1,59 @@
-import Head from 'next/head'
+import Head from "next/head"
 
-import BlockTitle from '../atoms/BlockTitle'
+import BlockTitle from "../atoms/BlockTitle"
 
 function generateJSONLD(questions) {
-  let mainEntity = []
-  questions.map(question => {
-    mainEntity.push({
-      "@type": "Question",
-      "name": question.name,
-      "acceptedAnswer": {
-        "@type": "Answer",
-        "text": question.text,
-      }
-    })
-  })
-  const jsonld = {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    "mainEntity": mainEntity,
-  }
-  return JSON.stringify(jsonld)
+	let mainEntity = []
+	questions.map((question) => {
+		mainEntity.push({
+			"@type": "Question",
+			name: question.name,
+			acceptedAnswer: {
+				"@type": "Answer",
+				text: question.text,
+			},
+		})
+	})
+	const jsonld = {
+		"@context": "https://schema.org",
+		"@type": "FAQPage",
+		mainEntity: mainEntity,
+	}
+	return JSON.stringify(jsonld)
 }
 
 function Item({ question, answer }) {
-  return (
-    <div className="mx-4">
-      <p className="text-lg font-bold md:text-xl">{question}</p>
-      <p className="mt-3 text-justify">{answer}</p>
-    </div>
-  )
+	return (
+		<div className="mx-4">
+			<p className="text-lg font-bold md:text-xl">{question}</p>
+			<p className="mt-3 text-justify">{answer}</p>
+		</div>
+	)
 }
 
 export default function BlockFaq({ questions }) {
-  return (
-    <>
-      <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: generateJSONLD(questions) }}
-        />
-      </Head>
+	return (
+		<>
+			<Head>
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{ __html: generateJSONLD(questions) }}
+				/>
+			</Head>
 
-      <section id="faq" className="py-10 md:py-20">
-        <div className="md:container md:mx-auto">
-          <BlockTitle
-                title="Les réponses à tes questions"
-                iconPicture={{ backgroundImage: "url('faq.png')" }}
-            />
-          <div className="grid grid-cols-1 gap-8 mx-2 lg:grid-cols-2">
-            {questions.map((item, index) => (
-              <Item key={index} question={item.name} answer={item.text} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
-  )
+			<section id="faq" className="py-10 md:py-20">
+				<div className="md:container md:mx-auto">
+					<BlockTitle
+						title="Les réponses à tes questions"
+						iconPicture={{ backgroundImage: "url('faq.png')" }}
+					/>
+					<div className="grid grid-cols-1 gap-8 mx-2 lg:grid-cols-2">
+						{questions.map((item, index) => (
+							<Item key={index} question={item.name} answer={item.text} />
+						))}
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
