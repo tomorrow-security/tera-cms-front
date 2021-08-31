39c6445d28e7b01ab87209207fe6e082bdfa7e6c
Alexia <alexia.skrzypczak@gmail.com>
2021-08-31 17:59:20 +0200

    statut 500 WIP

--- components/organisms/BlockAgenda.js
+++ components/organisms/BlockAgenda.js
@@ -1,34 +1,48 @@
-import axios from 'axios'
+import axios from "axios"
 
-import PageLink from '../atoms/PageLink'
-import SocialLink from '../atoms/SocialLink'
-import BlockTitle from '../atoms/BlockTitle'
-import SocialNav from '../molecules/SocialNav'
-import EventLive from '../molecules/EventLive'
-import IconeYoutube from '../atoms/IconeYoutube'
-
-
+import PageLink from "../atoms/PageLink"
+import SocialLink from "../atoms/SocialLink"
+import BlockTitle from "../atoms/BlockTitle"
+import SocialNav from "../molecules/SocialNav"
+import EventLive from "../molecules/EventLive"
+import IconeYoutube from "../atoms/IconeYoutube"
 
 export default function BlockAgenda(data) {
-  const events = axios.get('/api/agenda', data)
-  console.log(data)
-  /**
-   * bolean of no event in the array
-   */
-  const notEvent = events.length === 0
-  /**
-   * calendar cache if no event in the array
-   */
-  const invisible = notEvent ? 'hidden' : ''
+	const events = axios.post("/api/agenda", data)
+	console.log("data :", events)
+
+	/**
+	 * bolean of no event in the array
+	 */
+	const notEvent = events.length === 0
+	/**
+	 * calendar cache if no event in the array
+	 */
+	const invisible = notEvent ? "hidden" : ""
 
 	return (
 		<section id="agenda" className="py-10 lg:py-20">
 			<div className="md:container md:mx-auto">
-				<BlockTitle title="Agenda" iconPicture={{ backgroundImage: "url('pop-corn.png')" }} />
+				<BlockTitle
+					title="Agenda"
+					iconPicture={{ backgroundImage: "url('pop-corn.png')" }}
+				/>
 				<div className="lg:flex lg:flex-row lg:flex-nowrap lg:justify-around lg:items-center lg:text-xl">
 					<div className={`w-full lg:w-1/2 ${invisible}`}>
-            <ul className="flex flex-col justify-center mx-2 sm:justify-start lg:text-lg ">
-              Test
+						<ul className="flex flex-col justify-center mx-2 sm:justify-start lg:text-lg ">
+							{/* {events.map((event, index) => (
+								<EventLive
+									key={index}
+									url={event.url}
+									platform={event.platform}
+									dateTimeDate={event.timetable}
+									date={event.date}
+									dateTimeTimetable={event.dateTimeTimetable}
+									timetable={event.timetable}
+									description={event.description}
+									title={event.title}
+								/>
+							))} */}
 							{/* {
 								events.map((event, index) => (
 									<EventLive
@@ -44,37 +58,35 @@ export default function BlockAgenda(data) {
 									/>
                 ))
               } */}
-            </ul>
-          </div>
-          <div className="flex flex-col justify-center mt-8 space-y-10">
-            <p className="flex flex-col justify-center mx-auto space-y-2 font-bold text-center lg:text-2xl">
-              <span className="animate-wiggle">Abonne toi&nbsp;</span>
-              <span>et&nbsp;</span>
-              <span>suis en direct toutes nos actualités :</span>
-            </p>
-            <SocialNav />
-          </div>
-        </div>
-        <div className="flex flex-row flex-wrap items-center justify-center mt-8 text-base lg:mt-12 lg:text-xl">
-          <div className="flex">
-            <p className="w-max">Retouve tous&nbsp;</p>
-            <div>
-              {/* // TODO faire la page des live passés */}
-              <PageLink
-                id="pastlive"
-                label="nos lives passés"
-              />
-            </div>
-            <p className="w-max">&nbsp;sur notre chaîne</p>
-          </div>
-          <div className="m-2">
-            <SocialLink
-              name="Youtube"
-              url="https://www.youtube.com/channel/UCwP2ZwND8Ce4U0qpz03iSeg" logo={<IconeYoutube/>}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+						</ul>
+					</div>
+					<div className="flex flex-col justify-center mt-8 space-y-10">
+						<p className="flex flex-col justify-center mx-auto space-y-2 font-bold text-center lg:text-2xl">
+							<span className="animate-wiggle">Abonne toi&nbsp;</span>
+							<span>et&nbsp;</span>
+							<span>suis en direct toutes nos actualités :</span>
+						</p>
+						<SocialNav />
+					</div>
+				</div>
+				<div className="flex flex-row flex-wrap items-center justify-center mt-8 text-base lg:mt-12 lg:text-xl">
+					<div className="flex">
+						<p className="w-max">Retouve tous&nbsp;</p>
+						<div>
+							{/* // TODO faire la page des live passés */}
+							<PageLink id="pastlive" label="nos lives passés" />
+						</div>
+						<p className="w-max">&nbsp;sur notre chaîne</p>
+					</div>
+					<div className="m-2">
+						<SocialLink
+							name="Youtube"
+							url="https://www.youtube.com/channel/UCwP2ZwND8Ce4U0qpz03iSeg"
+							logo={<IconeYoutube />}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
--- pages/api/agenda.js
+++ pages/api/agenda.js
@@ -1,27 +1,37 @@
 import axios from "axios"
 
 export default async (req, res) => {
+	const data = JSON.stringify({
+		date4: timedate,
+		text: speaker,
+		statut_16: type,
+		statut_1: platform,
+		statut: statut,
+		texte_1: url,
+	})
+
 	// const query = `boards(ids: ${process.env.MONDAY_PLANNING_LIVES_BOARD_ID}) {
 	const query = `boards(ids: 1606107159) {
     items(limit: 3){
       name
       id
-      column_values (ids: ["date4","text","statut_16","statut_1","statut_18", "statut"]) {
+      column_values (ids: ["date4", "text,"statut_16","statut_1","statut_18", "statut", "texte_1"]) {
         id
+        title
         text
       }
     }`
 
-	const response = await axios.get(
-		"https://api.monday.com/v2",
-		JSON.stringify({ query: query }),
-		{
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: process.env.MONDAY_API_KEY,
-			},
-		}
-	)
+	response = await fetch("https://api.monday.com/v2", {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: process.env.MONDAY_API_KEY,
+		},
+		body: JSON.stringify({
+			query: query,
+		}),
+	})
 
 	res.status(response.status)
 }
