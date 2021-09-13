2e8c25608d0ec3298ee9eae01882f1fe439c1b9f
Alexia <alexia.skrzypczak@gmail.com>
2021-09-13 16:16:55 +0200

    prettier with space

--- components/molecules/InputButton.js
+++ components/molecules/InputButton.js
@@ -1,87 +1,87 @@
 import { useEffect } from "react"
 
 export default function InputButton({
-	mutation,
-	defaultValue,
-	loadingValue,
-	successValue,
-	onClick,
+  mutation,
+  defaultValue,
+  loadingValue,
+  successValue,
+  onClick,
 }) {
-	const submitButtonParams = {
-		loading: {
-			value: loadingValue,
-			color: "bg-tc-red-medium cursor-wait",
-			position: "absolute left-0 top-0 z-20",
-			shadow:
-				"bg-tc-red-xlight border border-tc-red-dark cursor-wait border-t-0",
-			disabled: true,
-		},
-		success: {
-			value: successValue,
-			color: "bg-tc-blue-medium cursor-not-allowed",
-			position: "absolute left-0 -top-10/100 z-20",
-			shadow:
-				"bg-tc-blue-xlight border border-tc-blue cursor-not-allowed border-t-0",
-			disabled: true,
-		},
-		default: {
-			value: defaultValue,
-			color:
-				"bg-tc-blue group-hover:bg-tc-red shadow cursor-pointer group-hover:shadow-none transition-colors duration-500 group-hover:-top-20/100 transform active:translate-y-20/100",
-			position: "absolute left-0 -top-15/100 z-20",
-			shadow:
-				"bg-tc-blue-light border border-tc-blue-dark group-hover:bg-tc-red-light group-hover:border-tc-red-dark shadow group-hover:shadow-none cursor-pointer  transition-colors duration-500",
-			disabled: false,
-		},
-	}
+  const submitButtonParams = {
+    loading: {
+      value: loadingValue,
+      color: "bg-tc-red-medium cursor-wait",
+      position: "absolute left-0 top-0 z-20",
+      shadow:
+        "bg-tc-red-xlight border border-tc-red-dark cursor-wait border-t-0",
+      disabled: true,
+    },
+    success: {
+      value: successValue,
+      color: "bg-tc-blue-medium cursor-not-allowed",
+      position: "absolute left-0 -top-10/100 z-20",
+      shadow:
+        "bg-tc-blue-xlight border border-tc-blue cursor-not-allowed border-t-0",
+      disabled: true,
+    },
+    default: {
+      value: defaultValue,
+      color:
+        "bg-tc-blue group-hover:bg-tc-red shadow cursor-pointer group-hover:shadow-none transition-colors duration-500 group-hover:-top-20/100 transform active:translate-y-20/100",
+      position: "absolute left-0 -top-15/100 z-20",
+      shadow:
+        "bg-tc-blue-light border border-tc-blue-dark group-hover:bg-tc-red-light group-hover:border-tc-red-dark shadow group-hover:shadow-none cursor-pointer  transition-colors duration-500",
+      disabled: false,
+    },
+  }
 
-	/** Reset button status
-	 * on successful submission */
-	useEffect(() => {
-		if (mutation.status === "success") {
-			mutation.reset()
-		}
-	})
+  /** Reset button status
+   * on successful submission */
+  useEffect(() => {
+    if (mutation.status === "success") {
+      mutation.reset()
+    }
+  })
 
-	return (
-		<div className="pt-2 mx-auto w-max group">
-			<div
-				className={`relative z-0 h-10 px-4 font-bold text-transparent rounded-t outline-none w-max rounded-b-xl ${
-					["loading", "success"].includes(mutation.status)
-						? submitButtonParams[mutation.status].shadow
-						: submitButtonParams["default"].shadow
-				}`}
-			>
-				{["loading", "success"].includes(mutation.status)
-					? submitButtonParams[mutation.status].value
-					: submitButtonParams["default"].value}
-				<input
-					onClick={onClick}
-					// onSubmit={() => reset({ keepIsSubmitted:false })}  //!
-					type="submit"
-					value={
-						["loading", "success"].includes(mutation.status)
-							? submitButtonParams[mutation.status].value
-							: submitButtonParams["default"].value
-					}
-					className={`${
-						["loading", "success"].includes(mutation.status)
-							? submitButtonParams[mutation.status].position
-							: submitButtonParams["default"].position
-					} 
+  return (
+    <div className="pt-2 mx-auto w-max group">
+      <div
+        className={`relative z-0 h-10 px-4 font-bold text-transparent rounded-t outline-none w-max rounded-b-xl ${
+          ["loading", "success"].includes(mutation.status)
+            ? submitButtonParams[mutation.status].shadow
+            : submitButtonParams["default"].shadow
+        }`}
+      >
+        {["loading", "success"].includes(mutation.status)
+          ? submitButtonParams[mutation.status].value
+          : submitButtonParams["default"].value}
+        <input
+          onClick={onClick}
+          // onSubmit={() => reset({ keepIsSubmitted:false })}  //!
+          type="submit"
+          value={
+            ["loading", "success"].includes(mutation.status)
+              ? submitButtonParams[mutation.status].value
+              : submitButtonParams["default"].value
+          }
+          className={`${
+            ["loading", "success"].includes(mutation.status)
+              ? submitButtonParams[mutation.status].position
+              : submitButtonParams["default"].position
+          } 
           w-max px-4 h-10 mx-auto outline-none text-white font-bold rounded-t rounded-b-xl
           ${
-						["loading", "success"].includes(mutation.status)
-							? submitButtonParams[mutation.status].color
-							: submitButtonParams["default"].color
-					}`}
-					disabled={
-						["loading", "success"].includes(mutation.status)
-							? submitButtonParams[mutation.status].disabled
-							: submitButtonParams["default"].disabled
-					}
-				/>
-			</div>
-		</div>
-	)
+            ["loading", "success"].includes(mutation.status)
+              ? submitButtonParams[mutation.status].color
+              : submitButtonParams["default"].color
+          }`}
+          disabled={
+            ["loading", "success"].includes(mutation.status)
+              ? submitButtonParams[mutation.status].disabled
+              : submitButtonParams["default"].disabled
+          }
+        />
+      </div>
+    </div>
+  )
 }
--- pages/enrolment/index.js
+++ pages/enrolment/index.js
@@ -22,159 +22,159 @@ import PageLink from "../../components/atoms/PageLink"
 
 // TODO importer la molecule EnrolmentForm à la place
 const EnrolmentForm = ({ mutation, onSubmit }) => {
-	const {
-		reset,
-		register,
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm()
+  const {
+    reset,
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm()
 
-	useEffect(() => {
-		reset()
-	}, [])
+  useEffect(() => {
+    reset()
+  }, [])
 
-	// TODO style au focus du phoneInput border à enlever à l'intérieur
-	return (
-		<form className="w-full mx-2" onSubmit={handleSubmit(onSubmit)}>
-			<div>
-				<div>
-					<div className="space-y-1 xl:space-y-8 xl:flex xl:flex-col xl:items-center ">
-						<div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
-							<div className="flex items-center space-y-1 xl:w-45%">
-								<label htmlFor="gender" className="w-30%">
-									Civilité* :
-								</label>
-								<InputRadio
-									name="gender"
-									control={control}
-									choices={[
-										{ label: "Mme", value: "F" },
-										{ label: "M.", value: "M" },
-									]}
-								/>
-								<span className=" text-tc-red">{errors?.gender?.message}</span>
-							</div>
-							<div className="flex items-center space-y-1 xl:w-45%">
-								<label htmlFor="email" className="w-30%">
-									E-mail* :
-								</label>
-								<input
-									id="email"
-									name="email"
-									type="email"
-									placeholder="thomas.anderson@tera-campus.com"
-									className={`p-2 flex-1 border rounded mx-2 outline-none ${
-										errors.email ? "border-tc-red" : "border-black"
-									}`}
-									{...register("email", { required: true })}
-								/>
-								<span className="text-tc-red">{errors?.email?.message}</span>
-							</div>
-						</div>
-						<div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
-							<div className="flex items-center space-y-1 xl:w-45%">
-								<label htmlFor="lastName" className="w-30%">
-									Nom* :
-								</label>
-								<input
-									id="lastName"
-									name="lastName"
-									type="text"
-									placeholder="Anderson"
-									className={`mx-2 p-2 flex-1 border rounded outline-none ${
-										errors.lastName ? "border-tc-red" : "border-black"
-									}`}
-									{...register("lastName", { required: true })}
-								/>
-								<span className="text-tc-red">{errors?.lastName?.message}</span>
-							</div>
-							<div className="flex items-center space-y-1 xl:w-45%">
-								<label htmlFor="firstName" className="w-30%">
-									Prénom* :
-								</label>
-								<input
-									id="firstName"
-									name="firstName"
-									type="text"
-									placeholder="Thomas"
-									className={`mx-2 p-2 flex-1 border rounded outline-none ${
-										errors.firstName ? "border-tc-red" : "border-black"
-									}`}
-									{...register("firstName", { required: true })}
-								/>
-								<span className="text-tc-red">
-									{errors?.firstName?.message}
-								</span>
-							</div>
-						</div>
-						<div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
-							<div className="space-y-1 xl:w-45%">
-								<div className="flex items-center ">
-									<label htmlFor="phone" className="w-30%">
-										Téléphone* :
-									</label>
-									{/* //TODO voir pour retirer le border intern au focus */}
-									<PhoneInputWithCountry
-										id="phone"
-										name="phone"
-										defaultCountry="FR"
-										international
-										withCountryCallingCode
-										control={control}
-										rules={{ required: true }}
-										className={`mx-2 p-2 flex-1 border w-65% rounded outline-none ${
-											errors.phone ? "border-tc-red" : "border-black"
-										}`}
-									/>
-								</div>
-							</div>
-						</div>
-						<label className="flex flex-col justify-center">
-							<div className="flex flex-row pt-8 mx-auto flex-nowrap w-max-full">
-								<input
-									id="consent"
-									name="consent"
-									type="checkbox"
-									className="my-auto mr-2 border outline-none "
-									{...register("consent", { required: true })}
-								/>
-								<div>
-									<div className="box-border flex flex-row flex-wrap items-center max-w-full text-xs sm:text-base">
-										<p className="border-b-2 border-transparent">
-											J'ai lu et j'accepte la&nbsp;
-										</p>
-										<PageLink
-											id="privacy"
-											label="politique de confidentialité"
-										/>
-										<p className="border-b-2 border-transparent">
-											, et j'accepte d'être recontacté par Tera Campus pour
-											finaliser mon inscription.
-										</p>
-									</div>
-								</div>
-							</div>
-							{errors.consent ? (
-								<span className="text-center text-tc-red">
-									Tu dois accepter la politique de confidentialité pour
-									continuer ton inscription.
-								</span>
-							) : null}
-						</label>
-					</div>
-				</div>
-			</div>
-			<div className="flex items-center justify-center my-4 xl:my-20">
-				<InputButton
-					defaultValue="Envoyer"
-					loadingValue="En cours d'envoi' ..."
-					successValue="Envoyé"
-					mutation={mutation}
-				/>
-			</div>
-		</form>
-	)
+  // TODO style au focus du phoneInput border à enlever à l'intérieur
+  return (
+    <form className="w-full mx-2" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <div>
+          <div className="space-y-1 xl:space-y-8 xl:flex xl:flex-col xl:items-center ">
+            <div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
+              <div className="flex items-center space-y-1 xl:w-45%">
+                <label htmlFor="gender" className="w-30%">
+                  Civilité* :
+                </label>
+                <InputRadio
+                  name="gender"
+                  control={control}
+                  choices={[
+                    { label: "Mme", value: "F" },
+                    { label: "M.", value: "M" },
+                  ]}
+                />
+                <span className=" text-tc-red">{errors?.gender?.message}</span>
+              </div>
+              <div className="flex items-center space-y-1 xl:w-45%">
+                <label htmlFor="email" className="w-30%">
+                  E-mail* :
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="thomas.anderson@tera-campus.com"
+                  className={`p-2 flex-1 border rounded mx-2 outline-none ${
+                    errors.email ? "border-tc-red" : "border-black"
+                  }`}
+                  {...register("email", { required: true })}
+                />
+                <span className="text-tc-red">{errors?.email?.message}</span>
+              </div>
+            </div>
+            <div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
+              <div className="flex items-center space-y-1 xl:w-45%">
+                <label htmlFor="lastName" className="w-30%">
+                  Nom* :
+                </label>
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  placeholder="Anderson"
+                  className={`mx-2 p-2 flex-1 border rounded outline-none ${
+                    errors.lastName ? "border-tc-red" : "border-black"
+                  }`}
+                  {...register("lastName", { required: true })}
+                />
+                <span className="text-tc-red">{errors?.lastName?.message}</span>
+              </div>
+              <div className="flex items-center space-y-1 xl:w-45%">
+                <label htmlFor="firstName" className="w-30%">
+                  Prénom* :
+                </label>
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  placeholder="Thomas"
+                  className={`mx-2 p-2 flex-1 border rounded outline-none ${
+                    errors.firstName ? "border-tc-red" : "border-black"
+                  }`}
+                  {...register("firstName", { required: true })}
+                />
+                <span className="text-tc-red">
+                  {errors?.firstName?.message}
+                </span>
+              </div>
+            </div>
+            <div className="xl:flex xl:justify-between xl:w-4/5 xl:flex-nowrap">
+              <div className="space-y-1 xl:w-45%">
+                <div className="flex items-center ">
+                  <label htmlFor="phone" className="w-30%">
+                    Téléphone* :
+                  </label>
+                  {/* //TODO voir pour retirer le border intern au focus */}
+                  <PhoneInputWithCountry
+                    id="phone"
+                    name="phone"
+                    defaultCountry="FR"
+                    international
+                    withCountryCallingCode
+                    control={control}
+                    rules={{ required: true }}
+                    className={`mx-2 p-2 flex-1 border w-65% rounded outline-none ${
+                      errors.phone ? "border-tc-red" : "border-black"
+                    }`}
+                  />
+                </div>
+              </div>
+            </div>
+            <label className="flex flex-col justify-center">
+              <div className="flex flex-row pt-8 mx-auto flex-nowrap w-max-full">
+                <input
+                  id="consent"
+                  name="consent"
+                  type="checkbox"
+                  className="my-auto mr-2 border outline-none "
+                  {...register("consent", { required: true })}
+                />
+                <div>
+                  <div className="box-border flex flex-row flex-wrap items-center max-w-full text-xs sm:text-base">
+                    <p className="border-b-2 border-transparent">
+                      J'ai lu et j'accepte la&nbsp;
+                    </p>
+                    <PageLink
+                      id="privacy"
+                      label="politique de confidentialité"
+                    />
+                    <p className="border-b-2 border-transparent">
+                      , et j'accepte d'être recontacté par Tera Campus pour
+                      finaliser mon inscription.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {errors.consent ? (
+                <span className="text-center text-tc-red">
+                  Tu dois accepter la politique de confidentialité pour
+                  continuer ton inscription.
+                </span>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center my-4 xl:my-20">
+        <InputButton
+          defaultValue="Envoyer"
+          loadingValue="En cours d'envoi' ..."
+          successValue="Envoyé"
+          mutation={mutation}
+        />
+      </div>
+    </form>
+  )
 }
 
 const pageTitle = "Inscription - Tera Campus"
@@ -183,49 +183,49 @@ const pageUrl = "https://tera-campus.com/enrolment"
 const apiUrl = process.env.NEXT_PUBLIC_ARPETTE_URL
 
 export default function Enrolment() {
-	const router = useRouter()
+  const router = useRouter()
 
-	const mutation = useMutation(
-		(formData) => {
-			axios
-				.post(`${apiUrl}/enrolment/create`, formData)
-				.then(({ data }) => router.push(`/enrolment/${data.enrolment}`))
-				.catch((error) => console.log(error))
-		},
-		{ retry: 3 }
-	)
+  const mutation = useMutation(
+    (formData) => {
+      axios
+        .post(`${apiUrl}/enrolment/create`, formData)
+        .then(({ data }) => router.push(`/enrolment/${data.enrolment}`))
+        .catch((error) => console.log(error))
+    },
+    { retry: 3 }
+  )
 
-	const onSubmit = (formData) => mutation.mutate(formData)
+  const onSubmit = (formData) => mutation.mutate(formData)
 
-	return (
-		<>
-			<Head>
-				<title>{pageTitle}</title>
-				<link rel="icon" href="/favicon.ico" />
-				<meta name="description" content={pageDescription} />
-				<meta property="og:url" content={pageUrl} />
-				<meta property="og:description" content={pageDescription} />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={pageDescription} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:description" content={pageDescription} />
+      </Head>
 
-			<main className="mx-2 md:container md:mx-auto">
-				<section id="enrolment" className="py-10 md:py-20">
-					<div className="flex flex-col justify-between flex-grow md:container md:mx-auto">
-						<BlockTitle
-							title="Inscription"
-							iconPicture={{
-								backgroundImage: "url('inscription-red-into-blue.png')",
-							}}
-						/>
-					</div>
-					<div className="xl:my-12">
-						<EnrolmentForm
-							status={mutation.status}
-							onSubmit={onSubmit}
-							mutation={mutation}
-						/>
-					</div>
-				</section>
-			</main>
-		</>
-	)
+      <main className="mx-2 md:container md:mx-auto">
+        <section id="enrolment" className="py-10 md:py-20">
+          <div className="flex flex-col justify-between flex-grow md:container md:mx-auto">
+            <BlockTitle
+              title="Inscription"
+              iconPicture={{
+                backgroundImage: "url('inscription-red-into-blue.png')",
+              }}
+            />
+          </div>
+          <div className="xl:my-12">
+            <EnrolmentForm
+              status={mutation.status}
+              onSubmit={onSubmit}
+              mutation={mutation}
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
