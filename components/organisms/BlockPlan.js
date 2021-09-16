be6df45218728a72bba38ecce9c2c9fae3d1d4dc
Alexia <alexia.skrzypczak@gmail.com>
2021-09-16 12:46:29 +0200

    plan view mobile

--- components/organisms/BlockPlan.js
+++ components/organisms/BlockPlan.js
@@ -28,30 +28,35 @@ const pages4 = [
 
 export default function BlockPlan({ id, label }) {
   return (
-    <section id="plan" className="pt-4 mx-4 md:py-10 lg:py-20">
+    <section id="plan" className="px-4 pt-4 md:py-10 lg:py-20">
       <div className="md:container md:mx-auto">
         <BlockTitle
           title="Plan du site"
           iconPicture={{ backgroundImage: "url('plan.png')" }}
         />
-      </div>
-      <div className="flex space-x-3 justify-evenly md:container md:mx-auto">
-        <div className="mt-8 text-lg md:mt-12">
-          <NavList id={id} label={label} title="Notre école" pages={pages1} />
-        </div>
-        <div className="mt-8 text-lg md:mt-12">
-          <NavList id={id} label={label} title="Le programme" pages={pages2} />
-        </div>
-        <div className="mt-8 text-lg md:mt-12">
-          <NavList id={id} label={label} title="Alternance" pages={pages3} />
-        </div>
-        <div className="mt-8 text-lg md:mt-12">
-          <NavList
-            id={id}
-            label={label}
-            title="Règlement et mantions légales"
-            pages={pages4}
-          />
+        <div className="flex flex-col pt-2 pb-4 space-y-8 text-sm md:pt-6 md:space-x-2 md:space-y-0 md:flex-row md:flex-wrap ">
+          <div className="md:flex-1">
+            <NavList id={id} label={label} title="Notre école" pages={pages1} />
+          </div>
+          <div className="md:flex-1">
+            <NavList
+              id={id}
+              label={label}
+              title="Le programme"
+              pages={pages2}
+            />
+          </div>
+          <div className="md:flex-1">
+            <NavList id={id} label={label} title="Alternance" pages={pages3} />
+          </div>
+          <div className="md:flex-1">
+            <NavList
+              id={id}
+              label={label}
+              title="Règlement et mentions légales"
+              pages={pages4}
+            />
+          </div>
         </div>
       </div>
     </section>
