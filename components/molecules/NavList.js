561608cf8568e24a098e959cd0e1ebc79560a4f2
Alexia <alexia.skrzypczak@gmail.com>
2021-09-16 12:46:11 +0200

    plan view mobile

--- components/molecules/NavList.js
+++ components/molecules/NavList.js
@@ -2,8 +2,8 @@ import PlanLink from "../atoms/PlanLink"
 
 export default function NavList({ pages, title }) {
   return (
-    <nav className="my-8 text-sm">
-      <h3 className="pb-4 font-semibold">{title}</h3>
+    <nav>
+      <h3 className="pb-2 font-semibold">{title}</h3>
       <ul className="space-y-2">
         {pages.map(({ id, label }) => (
           <li key={`navmd-${id}`}>
