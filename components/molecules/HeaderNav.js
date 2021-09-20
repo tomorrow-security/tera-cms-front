fc33293e35dc607945eb2d1f5bed73a1b53fe256
Alexia <alexia.skrzypczak@gmail.com>
2021-09-20 16:33:34 +0200

    removal of the HashLink atom in HeaderNav

--- components/molecules/HeaderNav.js
+++ components/molecules/HeaderNav.js
@@ -1,37 +1,20 @@
-import PageLink from "../atoms/PageLink";
-import HashLink from "../atoms/HashLink";
-
-const links1 = [{ id: "agenda", label: "Agenda" }];
-
-const links2 = [{ id: "faq", label: "Faq" }];
+import PageLink from "../atoms/PageLink"
 
 const pages1 = [
+  { id: "#agenda", label: "Agenda" },
   { id: "career", label: "Carrières" },
   { id: "guides", label: "Nos guides" },
   { id: "program", label: "Programme" },
   { id: "brochure", label: "Brochure" },
   { id: "enrolment", label: "Inscription" },
   { id: "join", label: "Rejoins-nous" },
-];
-
-const pages2 = [{ id: "contact", label: "Contact" }];
-
-//TODO revoir les space-y en wiew mobile
+  { id: "#faq", label: "Faq" },
+  { id: "contact", label: "Contact" },
+]
 
 export default function HeaderNav({ click }) {
   return (
     <nav className="pr-2 text-lg text-center lg:text-base xl:text-lg lg:flex lg:justify-between ">
-      <ul className="space-y-2 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-center">
-        {links1.map(({ id, label }) => (
-          <li
-            key={`navmd-${id}`}
-            onClick={click}
-            className="mx-auto text-center w-max"
-          >
-            <HashLink id={id} label={label} />
-          </li>
-        ))}
-      </ul>
       <ul className="space-y-2 lg:mx-4 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-center">
         {pages1.map(({ id, label }) => (
           <li
@@ -43,28 +26,6 @@ export default function HeaderNav({ click }) {
           </li>
         ))}
       </ul>
-      <ul className="space-y-2 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-center">
-        {links2.map(({ id, label }) => (
-          <li
-            key={`navmd-${id}`}
-            onClick={click}
-            className="mx-auto text-center w-max"
-          >
-            <HashLink id={id} label={label} />
-          </li>
-        ))}
-      </ul>
-      <ul className="space-y-2 lg:mx-4 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-center">
-        {pages2.map(({ id, label }) => (
-          <li
-            key={`navmd-${id}`}
-            onClick={click}
-            className="mx-auto text-center w-max"
-          >
-            <PageLink id={id} label={label} />
-          </li>
-        ))}
-      </ul>
     </nav>
-  );
+  )
 }
