98ba243f18aa54a5ef837ade344aa2d0cf47c036
Alexia <alexia.skrzypczak@gmail.com>
2021-09-21 15:52:05 +0200

    1st alternation paragraph

--- components/organisms/BlockDayReleaseTraining.js
+++ components/organisms/BlockDayReleaseTraining.js
@@ -10,22 +10,25 @@ export default function BlockDayReleaseTraining() {
         />
         <div className="mx-4">
           <section>
-            <h2 className="pb-2 text-lg font-bold">
-              L'alternance chez <span className="uppercase">Tera Campus</span>
-            </h2>
-            <p>
-              Dès la troisième année (BAC +2/3), l'ensemble des formations Tera
-              Campus sont dispensées à 100% par alternance. Cette période est
-              rythmée sur un cycle hebdomadaire de{" "}
-              <em>
-                1 jour de cours en télé-présentiel et 4 jours en entreprise
-              </em>
-            </p>
-            <div className="w-full">
-              <img
-                src="/public/groupeordi.jpg"
-                alt="groupe devant un ordinateur"
-              />
+            <div className="flex flex-col md:flex-row md:justify-around lg:justify-evenly">
+              <div className="mx-4 md:mx-0 md:w-1/2 xl:w-1/3 ">
+                <h2 className="text-lg font-bold lg:text-xl 2xl:text-2xl">
+                  L'alternance chez{" "}
+                  <span className="uppercase">Tera Campus</span>
+                </h2>
+                <p className="my-6 lg:text-lg 2xl:text-xl">
+                  Dès la troisième année (BAC +2/3), l'ensemble des formations
+                  Tera Campus sont dispensées à 100% par alternance. Cette
+                  période est rythmée sur un cycle hebdomadaire de{" "}
+                  <em className="not-italic font-semibold">
+                    1 jour de cours en télé-présentiel et 4 jours en entreprise
+                  </em>
+                </p>
+              </div>
+              <div
+                className="w-full bg-center bg-no-repeat bg-cover sm:bg-contain h-72md:w-1/2 lg:w-1/3 md:h-auto md:mx-2"
+                style={{ backgroundImage: "url('groupe-ordi.jpg')" }}
+              ></div>
             </div>
           </section>
         </div>
