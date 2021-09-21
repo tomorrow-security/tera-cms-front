524462e5d63e483db81c8e86143d9ec85d0ed5b0
Alexia <alexia.skrzypczak@gmail.com>
2021-09-21 18:09:33 +0200

    fix

--- components/organisms/BlockTrainingReleaseDef.js
+++ components/organisms/BlockTrainingReleaseDef.js
@@ -1,6 +1,6 @@
 export default function BlockReleaseTrainingDef() {
   return (
-    <section id="BlockReleaseTrainingDef" className="py-5 lg:py-10">
+    <section id="BlockTrainingReleseFunding" className="py-5 lg:py-10">
       <div className="mx-4 md:container md:mx-auto">
         <h2 className="text-lg font-bold md:mx-6 lg:mx-12 lg:text-xl 2xl:text-2xl">
           L'alternance chez c'est quoi ?
