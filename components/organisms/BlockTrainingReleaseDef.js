800eb0cacfebea1d20abbe33d7215ce8da66ca75
Alexia <alexia.skrzypczak@gmail.com>
2021-09-21 18:05:25 +0200

    BlockTrainingReleseDef

---
+++ components/organisms/BlockTrainingReleaseDef.js
@@ -0,0 +1,20 @@
+export default function BlockReleaseTrainingDef() {
+  return (
+    <section id="BlockReleaseTrainingDef" className="py-5 lg:py-10">
+      <div className="mx-4 md:container md:mx-auto">
+        <h2 className="text-lg font-bold md:mx-6 lg:mx-12 lg:text-xl 2xl:text-2xl">
+          L'alternance chez c'est quoi ?
+        </h2>
+        <p className="my-6 md:mx-6 lg:text-lg lg:mx-12 2xl:text-xl">
+          Un système de formation qui est fondé sur une phase pratique et une
+          phase théorique qui alternent, permettant de se préparer plus
+          efficacement à un métier.
+        </p>
+        <p className="font-bold md:mx-6 lg:mx-12 text-tc-red">
+          Étudier gratuitement tout en étant salarié : la vraie force de
+          l’alternance !
+        </p>
+      </div>
+    </section>
+  )
+}
