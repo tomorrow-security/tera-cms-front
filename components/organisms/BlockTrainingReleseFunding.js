fa18f9fe981174977b48df24956081e64b414955
Alexia <alexia.skrzypczak@gmail.com>
2021-09-21 18:09:46 +0200

    BlockTrainingReleseFunding

---
+++ components/organisms/BlockTrainingReleseFunding.js
@@ -0,0 +1,19 @@
+export default function BlockTrainingReleseFunding() {
+  return (
+    <section id="BlockTrainingReleseFunding" className="py-2 lg:py-5">
+      <div className="mx-4 md:container md:mx-auto lg:text-lg 2xl:text-xl">
+        <p className="font-semibold md:mx-6 lg:mx-12">
+          Le coût total de la formation est pris en charge par l’entreprise.
+        </p>
+        <p className="my-4 md:mx-6 lg:mx-12 ">
+          Double statut : l’alternant profite du statut d’étudiant (formation,
+          réduction pour les activités culturelles, transports) et du statut
+          salarié (rémunération, congés payés, insertion professionnelle).
+        </p>
+        <p className="md:mx-6 lg:mx-12">
+          Contrats longue durée (de 6 à 24 mois).
+        </p>
+      </div>
+    </section>
+  )
+}
