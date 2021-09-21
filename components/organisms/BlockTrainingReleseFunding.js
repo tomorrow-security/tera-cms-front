3d779b5487c40b202ec29df4175eb859b2608905
Alexia <alexia.skrzypczak@gmail.com>
2021-09-21 18:25:09 +0200

    BlockTrainingReleseFunding

--- components/organisms/BlockTrainingReleseFunding.js
+++ components/organisms/BlockTrainingReleseFunding.js
@@ -1,7 +1,10 @@
 export default function BlockTrainingReleseFunding() {
   return (
-    <section id="BlockTrainingReleseFunding" className="py-2 lg:py-5">
-      <div className="mx-4 md:container md:mx-auto lg:text-lg 2xl:text-xl">
+    <section
+      id="BlockTrainingReleseFunding"
+      className="py-2 bg-opacity-25 lg:py-5 bg-tc-blue md:bg-transparent md:bg-opacity-1"
+    >
+      <div className="mx-4 md:container md:mx-auto lg:text-lg 2xl:text-xl md:bg-tc-blue md:bg-opacity-25 md:border md:border-tc-blue md:rounded md:border-opacity-50">
         <p className="font-semibold md:mx-6 lg:mx-12">
           Le coût total de la formation est pris en charge par l’entreprise.
         </p>
