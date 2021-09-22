afd67e7151278d7198aaf25545d1e3af0ed4d73e
Alexia <alexia.skrzypczak@gmail.com>
2021-09-22 18:21:39 +0200

    sub menu layout desktop/mobile WIP

--- components/molecules/HeaderSubNav.js
+++ components/molecules/HeaderSubNav.js
@@ -4,10 +4,10 @@ export default function HeaderSubNav({ click, pages, title }) {
   return (
     <nav className="relative cursor-pointer group">
       {/* // TODO faire fonctionner group-focus:block */}
-      <h3>{title}</h3>
-      <ul className="absolute z-30 invisible space-y-2 bg-opacity-75 border rounded opacity-0 top-full group-hover:opacity-100 group-hover:visible bg-tc-blue border-tc-red">
+      <h3 className="mx-2">{title}</h3>
+      <ul className="absolute z-30 invisible space-y-2 bg-opacity-75 border rounded opacity-0 lg:mt-2 left-full lg:top-full group-hover:opacity-100 group-hover:visible bg-tc-blue border-tc-red">
         {pages.map(({ id, label }) => (
-          <li key={`navmd-${id}`} onClick={click} className="w-max">
+          <li key={`navmd-${id}`} onClick={click} className="min-w-max">
             <PageLink id={id} label={label} />
           </li>
         ))}
