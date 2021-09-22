ae6113a852a1c6554ee3c2b2d1c64e4a4f13cf4e
Alexia <alexia.skrzypczak@gmail.com>
2021-09-22 15:49:04 +0200

    subnav view mobile WIP

--- components/molecules/HeaderSubNav.js
+++ components/molecules/HeaderSubNav.js
@@ -2,14 +2,15 @@ import PageLink from "../atoms/PageLink"
 
 export default function HeaderSubNav({ click, pages, title }) {
   return (
-    <nav className="pr-2 text-lg group ">
+    <nav className="pr-2 text-lg cursor-pointer group ">
+      // TODO faire fonctionner group-focus:block
       <h3 className="pb-2">{title}</h3>
-      <ul className="hidden space-y-1 group-hover:block lg:space-y-0">
+      <ul className="hidden space-y-1 group-focus:block lg:group-hover:block lg:space-y-0">
         {pages.map(({ id, label }) => (
           <li
             key={`navmd-${id}`}
             onClick={click}
-            className="hidden group-hover:block w-max"
+            className="hidden group-focus:block lg:group-hover:block w-max"
           >
             <PageLink id={id} label={label} />
           </li>
