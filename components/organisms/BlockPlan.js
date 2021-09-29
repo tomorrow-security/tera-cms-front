d95906661f8612fd2ff8128ffe8b51ec4677db36
Alexia <alexia.skrzypczak@gmail.com>
2021-09-29 15:50:53 +0200

    layout BlockPlan

--- components/organisms/BlockPlan.js
+++ components/organisms/BlockPlan.js
@@ -2,34 +2,34 @@ import BlockTitle from "../atoms/BlockTitle"
 import NavList from "../molecules/NavList"
 import PlanLink from "../atoms/PlanLink"
 
-const pages1 = [
+const ecole = [
   { id: "concept", label: "Tera campus c'est quoi ?" },
-  { id: "#", label: "Notre pédagogie" },
-  { id: "#", label: "Bivouac" },
-  { id: "#", label: "Conseil de perfectionnement" },
-  { id: "#", label: "Etudier en situatin de handicap" },
-  { id: "#", label: "Habilitation qualité" },
+  { id: "pedagogy", label: "Notre pédagogie" },
+  { id: "bivouac", label: "Bivouac" },
+  { id: "schoolboard", label: "Conseil de perfectionnement" },
+  { id: "handicap", label: "Etudier en situation de handicap" },
+  { id: "quality", label: "Habilitation qualité" },
   { id: "faq", label: "FAQ" },
 ]
-const pages2 = [
+const program = [
   { id: "program", label: "BAC+3 Administrateur des systèmes d'information" },
-  { id: "#", label: "Les partenaires" },
+  { id: "partner", label: "Les partenaires" },
   { id: "career", label: "Les carrières" },
 ]
-const pages3 = [
+const alternance = [
   { id: "blockreleasetraining", label: "C'est quoi l'alternance" },
 ]
-const pages4 = [
-  { id: "#", label: "CGV - Conditions Générales de Ventes" },
-  { id: "#", label: "CGA - Conditions Générales d'Achats" },
-  { id: "#", label: "CGI - Conditions Générales d'Inscription" },
+const legal = [
+  { id: "cgv", label: "CGV - Conditions Générales de Ventes" },
+  { id: "cga", label: "CGA - Conditions Générales d'Achats" },
+  { id: "cgi", label: "CGI - Conditions Générales d'Inscription" },
   { id: "#", label: "Charte de l'utilisation du systè_me d'informations (SI)" },
-  { id: "#", label: "Règlement intérieur" },
+  { id: "schoolrules", label: "Règlement intérieur" },
   { id: "legacy", label: "Mentions légales" },
-  { id: "#", label: "Règlement des études" },
+  { id: "studyrules", label: "Règlement des études" },
 ]
 
-const pages5 = [
+const autre = [
   { id: "#agenda", label: "agenda" },
   { id: "enrolment", label: "admission" },
   { id: "contact", label: "contact" },
@@ -43,38 +43,50 @@ export default function BlockPlan({ id, label }) {
           title="Plan du site"
           iconPicture={{ backgroundImage: "url('plan.png')" }}
         />
-        <div className="flex flex-col pt-2 pb-4 space-y-8 text-sm md:pt-6 md:space-x-2 md:space-y-0 md:flex-row md:flex-wrap ">
-          <div className="md:flex-1">
-            <NavList id={id} label={label} title="Notre école" pages={pages1} />
+        <div className="md:relative">
+          <div className="flex flex-col mt-2 mb-4 space-y-8 text-sm md:mt-6 md:space-x-2 md:space-y-0 md:flex-row md:flex-wrap md:justify-around">
+            <div className="md:w-1/5">
+              <NavList
+                id={id}
+                label={label}
+                title="Notre école"
+                pages={ecole}
+              />
+            </div>
+            <div className="md:w-1/5">
+              <NavList
+                id={id}
+                label={label}
+                title="Le programme"
+                pages={program}
+              />
+            </div>
+            <div className="md:w-1/5">
+              <NavList
+                id={id}
+                label={label}
+                title="Alternance"
+                pages={alternance}
+              />
+            </div>
+            <div className="md:w-1/5">
+              <NavList
+                id={id}
+                label={label}
+                title="Règlement et mentions légales"
+                pages={legal}
+              />
+            </div>
           </div>
-          <div className="md:flex-1">
-            <NavList
-              id={id}
-              label={label}
-              title="Le programme"
-              pages={pages2}
-            />
+          <div className="md:bottom-6 md:absolute md:mx-auto md:w-1/2 md:inset-x-0">
+            <ul className="space-y-2 font-semibold uppercase md:space-y-0 md:flex md:w-max-content md:text-center md:justify-evenly">
+              {autre.map(({ id, label }) => (
+                <li key={`${id}`}>
+                  <PlanLink id={id} label={label} />
+                </li>
+              ))}
+            </ul>
           </div>
-          <div className="md:flex-1">
-            <NavList id={id} label={label} title="Alternance" pages={pages3} />
-          </div>
-          <div className="md:flex-1">
-            <NavList
-              id={id}
-              label={label}
-              title="Règlement et mentions légales"
-              pages={pages4}
-            />
-          </div>
-        </div>
-        <div className="md:flex md:justify-center">
-          <ul className="space-y-2 font-semibold uppercase md:space-y-0 md:flex md:w-max-content md:text-center md:space-x-24">
-            {pages5.map(({ id, label }) => (
-              <li key={`navmd-${id}`}>
-                <PlanLink id={id} label={label} />
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </section>
