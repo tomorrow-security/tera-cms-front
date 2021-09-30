0bafa18bf7c27ca16407a975e49198e6d8b4f3ae
Alexia <alexia.skrzypczak@gmail.com>
2021-09-30 14:11:21 +0200

    BlockPrerequisite in program page WIP layout

---
+++ components/organisms/BlockPrerequisite.js
@@ -0,0 +1,29 @@
+import BlockTitle from "../atoms/BlockTitle"
+import LittleTitle from "../atoms/LittleTitle"
+
+export default function BlockPreRequisite() {
+  return (
+    <section id="programme" className="py-10 md:py-20">
+      <div className="md:container md:mx-auto">
+        <BlockTitle
+          title="Pré-requis"
+          iconPicture={{ backgroundImage: "url('pre-requis.png')" }}
+        />
+        <div className="mx-4">
+          <LittleTitle children="Accès à la formation" />
+          <ul className="pl-2 space-y-2 list-inside list-square">
+            <li>
+              Avoir validé une 2ème année (BAC+2) en Informatique ou une
+              formation équivalente en France ou à l’international.{" "}
+            </li>
+            <li>
+              Avoir un BAC et une expérience professionnelle à l’appui en
+              informatique. Uniquement pour les cas exceptionnels et après
+              l’étude du dossier par la Direction Pédagogique.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/program.js
+++ pages/program.js
@@ -1,24 +1,26 @@
 import Head from "next/head"
 import BlockProgram from "../components/organisms/BlockProgram"
+import BlockPreRequisite from "../components/organisms/BlockPreRequisite"
 
 const pageTitle = "Programme - Tera Campus"
 const pageDescription = "Notre programme"
 const pageUrl = "https://tera-campus.com/program"
 
 export default function Legal() {
-	return (
-		<>
-			<Head>
-				<title>{pageTitle}</title>
-				<link rel="icon" href="/favicon.ico" />
-				<meta name="description" content={pageDescription} />
-				<meta property="og:url" content={pageUrl} />
-				<meta property="og:description" content={pageDescription} />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={pageDescription} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:description" content={pageDescription} />
+      </Head>
 
-			<main>
-				<BlockProgram />
-			</main>
-		</>
-	)
+      <main>
+        <BlockProgram />
+        <BlockPreRequisite />
+      </main>
+    </>
+  )
 }
