2fad019e701f3427a16e8aae82f6e76ab51421de
Alexia <alexia.skrzypczak@gmail.com>
2021-10-07 17:50:47 +0200

    group-focus BlockEval

--- components/organisms/BlockEval.js
+++ components/organisms/BlockEval.js
@@ -2,13 +2,13 @@ export default function BlockEval() {
   // TODO faire fonctionner le group-focus
   return (
     <section className="py-10">
-      <div className="mx-4 group">
+      <button className="mx-4 group">
         <div className="flex justify-center ">
           <p className="p-4 font-semibold text-white rounded cursor-pointer bg-tc-blue w-max">
             Les modalités d'évaluation
           </p>
         </div>
-        <div className="hidden w-11/12 p-2 m-auto space-y-2 list-inside rounded group-focus:justify-center group-focus:flex lg:mx-auto">
+        <div className="hidden w-11/12 p-2 m-auto space-y-2 list-inside rounded group-focus:flex lg:mx-auto">
           <ul className="p-2 border rounded list-square w-max border-tc-blue">
             <li>Travaux de groupe ou individuels</li>
             <li>Projets techniques</li>
@@ -17,7 +17,7 @@ export default function BlockEval() {
             <li>Projets professionnels (alternance) </li>
           </ul>
         </div>
-      </div>
+      </button>
     </section>
   )
 }
