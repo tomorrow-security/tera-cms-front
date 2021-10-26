5172fc9d1f6a6fd84af5281986dd3657f7aa7726
Alexia <alexia.skrzypczak@gmail.com>
2021-10-26 15:58:38 +0200

    code cleaning

--- components/organisms/BlockContract.js
+++ components/organisms/BlockContract.js
@@ -1,8 +1,21 @@
-import Traineeship from "../molecules/Traineeship"
 import SubTitle from "../atoms/SubTitle"
-import Professionalization from "../molecules/Professionalization"
 import LittleTitle from "../atoms/LittleTitle"
 import Paragraphe from "../atoms/Paragraphe"
+import ExternalLink from "../molecules/ExternalLink"
+
+const publics = [
+  "Jeunes âgés de 16 à 25 ans révolus en complément de leur formation",
+  "Bénéficiaires du RSA",
+  "Bénéficiaires de l'allocation de solidarité spécifique (ASS)",
+  "Bénéficiaires de l'allocation aux adultes handicapés (AAH)",
+  "Personnes sortant d'un contrat unique d'insertion (CUI)",
+]
+
+const helps = [
+  "Demandeurs d'emploi de 26 ans et plus inscrits depuis plus d'un an à Pôle emploi.",
+  "Bénéficiaires du revenu de solidarité active (RSA), de l'allocation de solidarité spécifique (ASS) ou de l'allocation aux adultes handicapés (AAH)",
+  "Personnes sortant d'un contrat unique d'insertion (CUI)",
+]
 
 export default function BlockContract() {
   return (
@@ -13,8 +26,54 @@ export default function BlockContract() {
           iconPicture={{ backgroundImage: "url('contract.png')" }}
         />
         <div className="mx-4">
-          <Traineeship />
-          <Professionalization />
+          <section id="traineeship" className="py-5">
+            <LittleTitle children="Le contrat d'apprentissage" />
+            <Paragraphe>
+              <span className="font-semibold">Public concerné :&nbsp;</span>Les
+              jeunes âgés de 16 à 29 ans révolus. (Hors cas exceptionnels :
+              Apprentis préparant un diplôme ou titre supérieur à celui obtenu,
+              travailleurs handicapés ou encore toutes personnes ayant un projet
+              de création ou de reprise d’entreprise).
+            </Paragraphe>
+            <Paragraphe>
+              La durée moyenne d’un contrat d’apprentissage varie de 6 à 12 mois
+              et peut être prolongée jusqu’à 36 mois après accord préalable
+              entre le centre de formation et l’employeur. Les bénéficiaires et
+              la nature des qualifications sont obligatoirement définis par
+              accords collectifs préalables.
+            </Paragraphe>
+            <ExternalLink
+              title="Le contrat d'apprentissage"
+              url="https://travail-emploi.gouv.fr/formation-professionnelle/formation-en-alternance-10751/apprentissage/contrat-apprentissage"
+              aria="lien vers le site gouvernemental travail emploi"
+            />
+          </section>
+          <section id="professionalization" className="py-5">
+            <LittleTitle children="Le contrat de professionnalisation" />
+            <Paragraphe>
+              <span className="font-semibold">Public concerné :&nbsp;</span>
+              <ul className="list-inside list-square">
+                {publics.map((people, index) => (
+                  <li key={index}>{people}</li>
+                ))}
+              </ul>
+            </Paragraphe>
+            <Paragraphe>
+              La durée moyenne du contrat de professionnalisation varie de 6 à
+              12 mois et peut être prolongée jusqu’à 36 mois dans les cas
+              suivants :
+              <ul className="w-11/12 mx-auto list-inside list-square">
+                {helps.map((help, index) => (
+                  <li key={index}>{help}</li>
+                ))}
+              </ul>
+            </Paragraphe>
+            <ExternalLink
+              title="Le contrat de professionnalisation"
+              url="https://travail-emploi.gouv.fr/formation-professionnelle/formation-en-alternance-10751/contrat-de-professionnalisation"
+              aria="lien vers le site gouvernemental travail emploi"
+            />
+          </section>
           <LittleTitle children="Aides spécifiques" />
           <Paragraphe>
             Des aides spécifiques peuvent également être attribuées aux
--- components/organisms/BlockRelease.js
+++ components/organisms/BlockRelease.js
@@ -1,11 +1,93 @@
-import DayReleaseTraining from "../molecules/DayReleaseTraining"
-import ReleaseTrainingDef from "../molecules/ReleaseTrainingDef"
+import BlockTitle from "../atoms/BlockTitle"
+import LittleTitle from "../atoms/LittleTitle"
+import Paragraphe from "../atoms/Paragraphe"
+import Strong from "../atoms/Strong"
+import ExternalLink from "../molecules/ExternalLink"
 
 export default function BlockRelease() {
   return (
     <section id="blockrelease" className="py-5">
-      <DayReleaseTraining />
-      <ReleaseTrainingDef />
+      <section id="DayReleaseTraining" className="py-5">
+        <div className="md:container md:mx-auto">
+          <BlockTitle
+            title="L'alternance"
+            iconPicture={{ backgroundImage: "url('alternance.png')" }}
+          />
+          <section className="mt-10 md:flex md:flex-row md:justify-between md:items-center">
+            <div className="mx-4 md:w-1/2">
+              <LittleTitle children="L'alternance chez TERA CAMPUS" />
+              <Paragraphe>
+                Dès la troisième année (BAC +2/3), l’ensemble des formations
+                Tera Campus sont dispensées à 100% par alternance. Cette période
+                est rythmée sur un cycle hebdomadaire de&nbsp;
+                <em className="not-italic font-semibold">
+                  1 jour de cours en télé-présentiel et 4 jours en entreprise
+                </em>
+              </Paragraphe>
+            </div>
+            <div
+              className="w-screen bg-center bg-no-repeat bg-cover h-33vh sm:h-50vh md:bg-contain md:bg-right md:w-1/2"
+              style={{ backgroundImage: "url('groupe-ordi.jpg')" }}
+            ></div>
+          </section>
+        </div>
+      </section>
+      <section id="releasetrainingdef" className="py-5">
+        <div className="md:container md:mx-auto">
+          <div className="mx-4">
+            <LittleTitle children=" L'alternance c'est quoi ?" />
+            <Paragraphe>
+              Un système de formation qui est fondé sur une phase pratique et
+              une phase théorique qui alternent, permettant de se préparer plus
+              efficacement à un métier.
+              <div>
+                <Strong>
+                  Étudier gratuitement tout en étant salarié : la vraie force de
+                  l’alternance !
+                </Strong>
+              </div>
+            </Paragraphe>
+            <div className="my-2 md:m-6 lg:mx-12">
+              <section id="trainingreleasefunding">
+                <div className="p-2 border border-opacity-50 rounded md:container md:mx-auto lg:text-lg 2xl:text-xl md:bg-tc-blue md:bg-opacity-25 border-tc-blue">
+                  <p className="font-semibold">
+                    Le coût total de la formation est pris en charge par
+                    l’entreprise.
+                  </p>
+                  <Paragraphe className="mt-4">
+                    Avec un double statut, l’alternant profite du statut
+                    d’étudiant (formation, réduction pour les activités
+                    culturelles, transports) et du statut de salarié
+                    (rémunération, congés payés, insertion professionnelle).
+                  </Paragraphe>
+                  <Paragraphe>
+                    Contrats longue durée (de 6 à 24 mois).{" "}
+                  </Paragraphe>
+                </div>
+              </section>
+            </div>
+            <Paragraphe>
+              Durant cette période, l’alternant suit un ensemble de cours
+              techniques en télé-présentiel, lui permettant de restituer son
+              savoir au travers des missions professionnelles qui lui sont
+              confiées au sein de l’entreprise. Le lien ainsi créé entre
+              enseignement théorique et application en entreprise, permet
+              d’exploiter les acquis immédiatement. Dans cette démarche, notre
+              école s’engage à aider tous ses étudiants dans leur recherche
+              d’entreprise, et propose deux solutions : le contrat
+              d'apprentissage et le contrat de professionnalisation.
+              Connaissez-vous le plan “1 jeune, 1 solution” ? Une solution
+              d’Etat pour former, accompagner et faciliter l’insertion
+              professionnelle de tous les jeunes...
+            </Paragraphe>
+            <ExternalLink
+              title="#1jeune1solution"
+              url="https://www.1jeune1solution.gouv.fr/"
+              aria="lien vars le site gouvernemental 1jeune1solution"
+            />
+          </div>
+        </div>
+      </section>
     </section>
   )
 }
