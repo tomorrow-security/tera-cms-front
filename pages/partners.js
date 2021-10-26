5ef358faf66a8e08d3203265e32b2feefe9f8a57
Alexia <alexia.skrzypczak@gmail.com>
2021-10-26 14:01:04 +0200

    page  partners

--- pages/partners.js
+++ pages/partners.js
@@ -1,10 +1,12 @@
 import BlockPartners from "../components/organisms/BlockPartners"
+import BlockApproach from "../components/organisms/BlockApproach"
 
 export default function partners() {
   return (
     <section id="partners" className="py-5">
       <div className="md:container md:mx-auto">
         <BlockPartners />
+        <BlockApproach />
       </div>
     </section>
   )
