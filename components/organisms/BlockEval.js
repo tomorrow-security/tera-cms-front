a7ed03951677e0062c242215bf7d2760c12e4f0e
Alexia <alexia.skrzypczak@gmail.com>
2021-10-28 15:44:57 +0200

    fix & layout BlockEval

--- components/organisms/BlockEval.js
+++ components/organisms/BlockEval.js
@@ -8,16 +8,16 @@ const modes = [
 
 export default function BlockEval() {
   return (
-    <section className="py-5 md:py-10">
-      <div className="flex flex-col justify-center mx-4 md:w-1/2 lg:w-1/3 2xl:w-1/4 md:mx-auto">
-        <div className="border rounded border-tc-blue">
+    <section className="py-5">
+      <div className="flex flex-col justify-center mx-auto w-max">
+        <div className="border-2 rounded border-tc-blue">
           <div className="flex justify-center">
-            <p className="p-4 font-semibold text-white rounded cursor-pointer group-focus:rounded-t-none bg-tc-blue w-max">
+            <p className="w-full py-4 mx-4 font-semibold text-center text-white rounded-b bg-tc-blue">
               Les modalités d'évaluation
             </p>
           </div>
-          <div className="justify-center w-11/12 p-2 m-auto space-y-2 group-focus:flex lg:mx-auto">
-            <ul className="p-2 list-none w-max">
+          <div className="flex justify-center mx-auto space-y-2">
+            <ul className="p-4 list-none w-max">
               {modes.map((mode, index) => (
                 <li key={index}>{mode}</li>
               ))}
