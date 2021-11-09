0c26fbe7fa0d572f61a7e4f4aa3c2722181ea062
Alexia <alexia.skrzypczak@gmail.com>
2021-11-09 10:43:24 +0100

    page rate

--- pages/rate.js
+++ pages/rate.js
@@ -1,7 +1,14 @@
 import Head from "next/head"
-import PageLink from "../components/atoms/PageLink"
 
 import BlockTitle from "../components/organisms/BlockTitle"
+import SubTitle from "../components/atoms/SubTitle"
+import LitleTitle from "../components/atoms/LittleTitle"
+import Bullet from "../components/atoms/Bullet"
+import List from "../components/atoms/List"
+import Paragraphe from "../components/atoms/Paragraphe"
+import ParagrapheXl from "../components/atoms/ParagrapheXl"
+import Strong from "../components/atoms/Strong"
+import ExternalLink from "../components/molecules/ExternalLink"
 
 const pageTitle = "Tairfs - Tera Campus"
 const pageDescription = "Les tarifs"
@@ -22,6 +29,46 @@ export default function Bivouacs() {
             <BlockTitle
               title="Tarifs"
               iconPicture={{ backgroundImage: "url('tarifs.png')" }}
+            />
+            <SubTitle
+              title="Formation initial & alternance"
+              iconPicture={{ backgroundImage: "url('tarifs.png')" }}
+            />
+            <div className="md:w-1/4 md:border md:border-tc-blue md:rounded md:px-6 md:pb-6 md:mx-auto md:my-6">
+              <div className="font-semibold">
+                <Bullet>Aucun frais à prévoir pour :</Bullet>
+              </div>
+              <List>
+                <li>le test de positionnement</li>
+                <li>le test de positionnement</li>
+                <li>les frais de dossier</li>
+              </List>
+            </div>
+            {/* <LitleTitle>Frais de Scolarité 1 & 2 années</LitleTitle> */}
+            <LitleTitle>Payer en plusieurs fois :</LitleTitle>
+            <div className="md:w-11/12 md:mx-auto">
+              <Bullet>Alternance à partir de la 3ème année :</Bullet>
+              <Paragraphe>
+                Les 3 années de formation en alternance seront entièrement
+                financées par votre entreprise d’accueil !
+              </Paragraphe>
+              <ParagrapheXl>
+                Le reste à charge sera donc de 0 euro.
+              </ParagrapheXl>
+              <Paragraphe>
+                Tu seras en plus <Strong>rémunéré(e) mensuellement</Strong> pour
+                ton travail et en fonction de ta situation
+              </Paragraphe>
+              <Paragraphe>
+                Le choix du télé-présentiel, couplé au financement de ta
+                formation par l'entreprise, te permettra donc d'entamer ta
+                carrière sereinement, et <Strong>sans aucune dettes</Stron> !
+              </Paragraphe>
+            </div>
+            <ExternalLink
+              title="l'alternance"
+              url="https://www.alternance.emploi.gouv.fr/accueil"
+              aria="lien vers le site gouvernemental de l'alternance"
             />
           </div>
         </section>
