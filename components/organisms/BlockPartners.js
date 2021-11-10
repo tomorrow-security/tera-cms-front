53abac631065558fbe5bcf4678a3594a441afdd8
Alexia <alexia.skrzypczak@gmail.com>
2021-11-10 14:59:47 +0100

    layout BlockPartners

--- components/organisms/BlockPartners.js
+++ components/organisms/BlockPartners.js
@@ -8,28 +8,26 @@ import Paragraphe from "../atoms/Paragraphe"
 export default function BlockPartners() {
   return (
     <section id="partners" className="py-5">
-      <div className="md:container md:mx-auto">
-        <div className="mx-4">
-          <Approach />
-          <CarouselPartners partners={<PartnersEuropean />} />
-          <Paragraphe>
-            A travers ses partenaires,&nbsp; Tera Campus veut te donner
-            l&apos;opportunité d&apos;apprendre de tous les acteurs du
-            marché.&nbsp; Notre approche est conceptuelle :&nbsp;
-            <Strong>
-              nous ne nous focalisons pas sur des technologies
-              spécifiques,&nbsp;mais formons nos étudiants à savoir
-              s&apos;adapter à n&apos;importe laquelle.
-            </Strong>
-          </Paragraphe>
-          <div className="mx-auto mb-4 text-xl font-bold w-max md:mb-8"></div>
-          <Paragraphe>
-            Chez Tera Campus,&nbsp;il est possible d&apos;en apprendre plus sur
-            l&apos;écosystème technique européen&nbsp;et nous privilégions les
-            partenaires nationaux lorsque cela est possible.
-          </Paragraphe>
-          <CarouselPartners partners={<PartnersCollaboratives />} />
-        </div>
+      <div className="mx-4">
+        <Approach />
+        <CarouselPartners partners={<PartnersEuropean />} />
+        <Paragraphe>
+          A travers ses partenaires,&nbsp; Tera Campus veut te donner
+          l&apos;opportunité d&apos;apprendre de tous les acteurs du
+          marché.&nbsp; Notre approche est conceptuelle :&nbsp;
+          <Strong>
+            nous ne nous focalisons pas sur des technologies
+            spécifiques,&nbsp;mais formons nos étudiants à savoir s&apos;adapter
+            à n&apos;importe laquelle.
+          </Strong>
+        </Paragraphe>
+        <div className="mx-auto mb-4 text-xl font-bold w-max md:mb-8"></div>
+        <Paragraphe>
+          Chez Tera Campus,&nbsp;il est possible d&apos;en apprendre plus sur
+          l&apos;écosystème technique européen&nbsp;et nous privilégions les
+          partenaires nationaux lorsque cela est possible.
+        </Paragraphe>
+        <CarouselPartners partners={<PartnersCollaboratives />} />
       </div>
     </section>
   )
