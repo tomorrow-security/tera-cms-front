b2326615e7178780f632dd83bc631f94f468e5a2
Alexia <alexia.skrzypczak@gmail.com>
2021-11-16 16:56:51 +0100

    coloring of the bg of externalLink and InternalLink

--- components/molecules/InternalLink.js
+++ components/molecules/InternalLink.js
@@ -5,13 +5,19 @@ export default function InternalLink({ id, label, red, blue, whiteR, whiteB }) {
   return (
     <Link className="flex justify-center my-4 " href={`/${id}`}>
       <a
-        className={`px-6 py-2 ${
-          whiteR ? "border rounded border-tc-red text-tc-red bg-white" : ""
+        className={`px-6 py-2 transition-colors duration-700 ease-in-out ${
+          whiteR
+            ? "border rounded border-tc-red text-tc-red bg-white hover:bg-tc-blue"
+            : ""
         } ${
-          whiteB ? "border rounded border-tc-blue text-tc-blue bg-white" : ""
-        } ${red ? "border rounded border-white text-white bg-tc-red" : ""}${
-          blue ? "border-white text-white bg-tc-blue" : ""
-        } `}
+          whiteB
+            ? "border rounded border-tc-blue text-tc-blue bg-white hover:bg-tc-red hover:bg-opacity-50"
+            : ""
+        } ${
+          red
+            ? "border rounded border-white text-white bg-tc-red hover:bg-tc-blue"
+            : ""
+        }${blue ? "border-white text-white bg-tc-blue hover:bg-tc-red" : ""} `}
       >
         {label}
       </a>
