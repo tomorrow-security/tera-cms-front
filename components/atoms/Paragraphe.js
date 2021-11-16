b5936e32725aaedf899eb9c1bd38f788e1329ec5
Alexia <alexia.skrzypczak@gmail.com>
2021-11-16 16:57:36 +0100

    possibility to change the size of the Paragraph component

--- components/atoms/Paragraphe.js
+++ components/atoms/Paragraphe.js
@@ -1,5 +1,11 @@
-export default function Paragraphe({ children }) {
+export default function Paragraphe({ children, lg, xl, xxl }) {
   return (
-    <p className="my-6 md:mx-6 lg:text-lg lg:mx-12 2xl:text-xl">{children}</p>
+    <p
+      className={`my-6 md:mx-6 lg:mx-12 ${lg ? "lg:text-lg 2xl:text-xl" : ""} ${
+        xl ? "text-xl md:text-2xl 2xl:text-3xl" : ""
+      } ${xxl ? "text-2xl md:text-3xl 2xl:text-4xl" : ""}`}
+    >
+      {children}
+    </p>
   )
 }
