23644c8793a4371a38b0bda58b3140a01a461a3b
Alexia <alexia.skrzypczak@gmail.com>
2021-11-16 14:57:23 +0100

    setting props of the color for the Cta

--- components/atoms/Cta.js
+++ components/atoms/Cta.js
@@ -1,20 +1,15 @@
 import Link from "next/link"
-//TODO faire fonctionner le choix du style
-export default function Cta({ title, id, color }) {
-  if (({ color } = "white")) {
-    colorStyle = "bg-white border-white text-tc-blue hover:bg-tc-red"
-  }
-  if (({ color } = "red")) {
-    colorStyle = "bg-tc-red  border-tc-red text-white hover:bg-tc-blue"
-  }
-  if (({ color } = "blue")) {
-    colorStyle = "bg-tc-blue  border-tc-blue text-white hover:bg-tc-red"
-  }
-
+export default function Cta({ title, id, red, blue, white }) {
   return (
     <Link href={`${id}`} className="flex justify-center">
       <a
-        className={`${colorStyle} px-4 text-lg font-bold text-center transition-colors duration-700 ease-in-out border rounded w-max p-y-1 hover:text-opacity-75`}
+        className={`${
+          red ? "bg-tc-red  border-tc-red text-white hover:bg-tc-blue" : ""
+        } ${
+          blue ? "bg-tc-blue  border-tc-blue text-white hover:bg-tc-red" : ""
+        } ${
+          white ? "bg-white border-white text-tc-blue hover:bg-tc-red" : ""
+        } px-4 text-lg font-bold text-center transition-colors duration-700 ease-in-out border rounded w-max p-y-1 hover:text-opacity-75`}
       >
         {title}
       </a>
