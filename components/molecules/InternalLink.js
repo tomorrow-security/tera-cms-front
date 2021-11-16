de6c35ace3d4a2b53cd2ee84bbdf085bb06c2b6a
Alexia <alexia.skrzypczak@gmail.com>
2021-11-16 15:21:45 +0100

    setting color props for InternalLink

--- components/molecules/InternalLink.js
+++ components/molecules/InternalLink.js
@@ -1,18 +1,15 @@
 import React from "react"
 import Link from "next/link"
 
-export default function InternalLink({ id, label, background }) {
-  /**
-   * formatting the id for use in url route
-   */
-  const href = `/${id}`
-
-  const bgStyle = `bg-${background}`
-
+export default function InternalLink({ id, label, red, blue, white }) {
   return (
-    <Link className="flex justify-center my-4 " href={href}>
+    <Link className="flex justify-center my-4 " href={`/${id}`}>
       <a
-        className={`px-6 py-2 border rounded text-tc-red border-tc-red ${bgStyle}`}
+        className={`px-6 py-2 border rounded ${
+          red ? "border-tc-red text-tc-red bg-white" : ""
+        } ${blue ? "border-tc-blue text-tc-blue bg-white" : ""} ${
+          white ? "border-white text-white bg-tc-red" : ""
+        } `}
       >
         {label}
       </a>
