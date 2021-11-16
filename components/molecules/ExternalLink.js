a97aa62866af57d354ab7eb89b860e8f8f88cf76
Alexia <alexia.skrzypczak@gmail.com>
2021-11-16 15:17:45 +0100

    mise en props de la couleur pour ExternalLink

--- components/molecules/ExternalLink.js
+++ components/molecules/ExternalLink.js
@@ -1,15 +1,16 @@
-export default function ExternalLink({ title, url, aria }) {
+// TODO faire fonctionner visited
+
+export default function ExternalLink({ title, url, aria, red, blue, white }) {
   return (
     <div className="flex justify-center my-4">
-      {/* 
-        // TODO faire fonctionner visited 
-        */}
       <a
         href={url}
         target="_blank"
         rel="noreferrer"
         aria-label={aria}
-        className="px-6 py-2 border rounded border-tc-red text-tc-red"
+        className={`${red ? "border-tc-red text-tc-red" : ""} ${
+          blue ? "border-tc-blue text-tc-blue" : ""
+        } ${white ? "border-white text-white" : ""} px-6 py-2 border rounded`}
       >
         {title}
       </a>
