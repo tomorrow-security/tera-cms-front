ce74a1900a4f7727ebb0a696fb063e95bd28fdaf
Brice AIELLO <brice.aiello@me.com>
2021-12-12 19:16:34 +0100

    Create and use 2 cards components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import Head from "next/head"
+import Link from "next/link"
 
 import BlockAgenda from "../components/organisms/BlockAgenda"
 import BlockConcept from "../components/organisms/BlockConcept"
@@ -10,6 +11,50 @@ const pageTitle = "École supérieure d'informatique - Tera Campus"
 const pageDescription =
   "École supérieure d'informatique, Tera Campus propose des formations diplômantes Bac+3 et Bac+5 en développement informatique et infrastructure."
 const pageUrl = "https://tera-campus.com"
+
+function Card({ title, content }) {
+  return (
+    <div className="w-full bg-white rounded-3xl shadow-lg overflow-hidden">
+      <img src="https://source.unsplash.com/random/800x800?landscape" className="h-96 sm:rounded-t-3xl object-cover object-center w-full" />
+      <div className="p-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="mb-4 text-justify">{content}</p>
+      </div>
+    </div>
+  )
+}
+
+function CardOverImage({ imageUrl, imageAlt, chipPrimary, chipSecondary, title, content }) {
+  return (
+    <div className="antialiased text-gray-900">
+      <div>
+        <img
+          src={imageUrl}
+          alt={imageAlt}
+          className="w-full h-72 lg:rounded-lg lg:shadow-md object-cover object-center"
+        />
+        <div className="relative px-4 -mt-16">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <div className="flex justify-between items-baseline">
+              <span className="px-4 py-2 bg-tc-blue-navy text-white text-xs md:text-sm inline-block rounded-full uppercase font-semibold tracking-wide">
+                { chipPrimary }
+              </span>
+              <div className="ml-2 text-gray-600 text-xs md:text-sm font-semibold tracking-wider">
+                { chipSecondary }
+              </div>
+            </div>
+            <h4 className="mt-8 text-center text-xl font-bold uppercase leading-tight">
+              { title }
+            </h4>
+            <p className="mt-8 text-center truncate">
+              { content }
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function Index({ agenda, questions }) {
   return (
@@ -24,8 +69,72 @@ function Index({ agenda, questions }) {
 
       <main>
         <Hero />
+
         <BlockAgenda events={agenda} />
+
+        <section id="concept" className="pb-8 md:py-10 lg:py-20 bg-tc-lavender">
+          <div className="md:container md:mx-auto">
+            <div className="flex flex-col lg:flex-row gap-8">
+              <div className="w-full lg:w-1/2">
+                <CardOverImage
+                  imageUrl="https://source.unsplash.com/random/350x350?fuit"
+                  imageAlt="random image"
+                  chipPrimary="Bac +3"
+                  chipSecondary="Titre RNCP de niveau 6"
+                  title="Administrateur des Systèmes d'Information"
+                  content="Parcours en alternance"
+                />
+              </div>
+              <div className="w-full lg:w-1/2">
+                <CardOverImage
+                  imageUrl="https://source.unsplash.com/random/350x350?animal"
+                  imageAlt="random image"
+                  chipPrimary="Bac +5"
+                  chipSecondary="Titre RNCP de niveau 7"
+                  title="Expert des Systèmes d'Information"
+                  content="Parcours en alternance"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-tc-blue-navy">
+          <div className="py-24 md:container md:mx-auto">
+            <div className="text-center text-white">
+              <p className="mb-8 text-4xl font-bold">Avec l'alternance, étudier et être salarié c'est possible !</p>
+              <Link href="#">
+                <a className="p-4 border border-white rounded bg-white text-tc-blue-navy hover:bg-tc-blue-navy hover:text-white font-bold">Je me renseigne</a>
+              </Link>
+            </div>
+          </div>
+        </section>
+
+        <section id="bivouacs" className="py-4 md:py-10 lg:py-20 bg-tc-lavender">
+          <div className="md:container md:mx-auto">
+            <div className="p-4 lg:p-0 flex flex-col lg:flex-row-reverse gap-8">
+              {/* Left */}
+              <div className="w-full lg:w-1/2 flex flex-col justify-center text-justify lg:text-left">
+                <div className="text-lg lg:text-xl font-bold text-tc-red uppercase">Bivouacs</div>
+                <div className="mt-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">6 semaines complètes en présentiel</div>
+                <p className="mt-8">Avec Tera Campus vous partirez en aventure au mois d'octobre, janvier et mai. Ces séminaires sont entièrement financés par l'école.</p>
+                <p className="mt-4">Les lieux de rendez-vous des séminaires sont définis par Tera Campus à l'avance, et sont entièrement financés par l'école.</p>
+              </div>
+              {/* Left End */}
+              {/* Right */}
+              <div className="w-full lg:w-1/2 lg:px-16 xl:px-32 flex justify-center">
+                <Card
+                  title={`Souvenir d'un bivouac en Ardèche`}
+                  content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi tenetur, voluptatibus harum consequuntur alias quaerat excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe alias dolores dolorem assumenda totam."
+                />
+              </div>
+              {/* Right End */}
+            </div>
+          </div>
+        </section>
+
         <BlockConcept />
+
         <BlockFaq questions={questions} />
       </main>
     </>
