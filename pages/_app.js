038bef67ccaf22ddca3c7ea6168ac6ddb249ea3a
Brice AIELLO <brice.aiello@me.com>
2022-02-28 16:00:47 +0100

    Record first_touch source in cookies

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 
+import cookies from "cookie-cutter"
 import Head from "next/head"
+import { useRouter } from 'next/router'
+import { useEffect } from "react"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 import Analytics from "../componentsDraft/atoms/Analytics"
@@ -10,6 +13,14 @@ import Header from "../componentsDraft/organisms/Header"
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		if (!cookies.get('tc_ft_source')) cookies.set('tc_ft_source', router.query.utm_source || 'direct')
+		if (!cookies.get('tc_ft_medium')) cookies.set('tc_ft_medium', router.query.utm_medium || 'site-web')
+		if (!cookies.get('tc_ft_campaign')) cookies.set('tc_ft_campaign', router.query.utm_campaign || 'none')
+	}, [])
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Analytics>
