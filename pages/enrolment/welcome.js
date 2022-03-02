94f3b15ac50b11d0e71b1ea8c748d32adc6c1eb1
Brice AIELLO <brice.aiello@me.com>
2022-03-02 18:21:48 +0100

    Add image on enrolment welcome page

--- pages/enrolment/welcome.js
+++ pages/enrolment/welcome.js
@@ -26,17 +26,24 @@ export default function EnrolmentWelcome() {
           <div className="text-xl md:text-4xl font-bold">
             Merci pour votre pré-inscription chez Tera Campus !
           </div>
-          <div className="mt-8 text-lg md:text-2xl">
+          <p className="mt-8 text-lg md:text-2xl">
             Next step ?
-          </div>
-          <div className="mt-8">
-            Rendez-vous dans votre boîte e-mail pour passer le test de positionnement. Ce test
-            aura une durée de 2h et nous permettra d'identifier vos points forts et vos lacunes
-            pour vous proposer le meilleur accompagnement.
-          </div>
-          <div className="mt-8">
-            Bonne journée futur(e) champion(nne) de la tech !
-          </div>
+          </p>
+          <p className="mt-8">
+            Rendez-vous dans votre boîte e-mail pour passer le test de positionnement.<br />
+            Ce test sous forme de QCM comporte 100 questions sur plusieurs domaines de l'IT.
+          </p>
+          <p className="mt-8">
+            Objectif ? Identifier vos points forts et vos lacunes pour vous assurer le meilleur accompagnement.
+          </p>
+          <img
+            src="https://res.cloudinary.com/teracampus/image/upload/q_auto:low/v1638972870/cms/AdobeStock_233254061-removebg-preview_sje83s.png"
+            alt="Happuy dude"
+            className="float-right w-1/2 md:w-1/3 lg:w-1/4"
+          />
+          <p className="mt-8">
+           N'oubliez pas c'est un test de positionnement et non d'admission donc TAKE IT EASY !
+          </p>
         </Block>
       
       </main>
